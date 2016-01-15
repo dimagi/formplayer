e060889970a74bba0dff6096c3650ab68c238bbd
Will Pride <wpride@dimagi.com>
2016-01-15 17:19:00 -0500

    add session

---
+++ src/main/java/session/FormEntrySession.java
@@ -0,0 +1,66 @@
+package session;
+
+import org.commcare.api.json.WalkJson;
+import org.javarosa.core.model.FormDef;
+import org.javarosa.form.api.FormEntryController;
+import org.javarosa.form.api.FormEntryModel;
+import org.javarosa.xform.parse.XFormParser;
+import org.json.JSONArray;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.UUID;
+
+/**
+ * Created by willpride on 1/15/16.
+ */
+public class FormEntrySession {
+
+    private FormDef formDef;
+    private FormEntryModel formEntryModel;
+    private FormEntryController formEntryController;
+
+    String title;
+    String[] langs;
+    String uuid;
+
+    public FormEntrySession(String formXml, String initLang) throws IOException {
+        formDef = parseFormDef(formXml);
+        formEntryModel = new FormEntryModel(formDef, FormEntryModel.REPEAT_STRUCTURE_LINEAR);
+        formEntryController = new FormEntryController(formEntryModel);
+        formEntryController.setLanguage(initLang);
+        title = formDef.getTitle();
+        langs = formEntryModel.getLanguages();
+        uuid = UUID.randomUUID().toString();
+    }
+
+    private FormDef parseFormDef(String formXml) throws IOException {
+        XFormParser mParser = new XFormParser(new StringReader(formXml));
+        return mParser.parse();
+    }
+
+    public FormEntryModel getFormEntryModel(){
+        return formEntryModel;
+    }
+
+    public FormEntryController getFormEntryController(){
+        return formEntryController;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String[] getLanguages(){
+        return langs;
+    }
+
+    public JSONArray getFormTree() {
+        JSONArray ret = WalkJson.walkToJSON(getFormEntryModel(), getFormEntryController());
+        return ret;
+    }
+
+    public String getUUID(){
+        return uuid;
+    }
+}
