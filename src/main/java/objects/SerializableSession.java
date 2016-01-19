8c992c8de6e09636b34243e250406854e8c6c04e
Will Pride <wpride@dimagi.com>
2016-01-19 17:01:17 -0500

    redis

---
+++ src/main/java/objects/SerializableSession.java
@@ -0,0 +1,46 @@
+package objects;
+
+import java.io.Serializable;
+
+/**
+ * Created by willpride on 1/19/16.
+ */
+public class SerializableSession implements Serializable{
+    private String id;
+    private String instanceXml;
+
+
+    public String getInstanceXml() {
+        return instanceXml;
+    }
+
+    public void setInstanceXml(String instanceXml) {
+        this.instanceXml = instanceXml;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode(){
+        return id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof SerializableSession){
+            return obj.hashCode() == hashCode();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return "Session [id=" + id + ", instance=" + instanceXml + "]";
+    }
+}
