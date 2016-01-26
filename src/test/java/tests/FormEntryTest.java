f0c73770d65eff0c905ca591bbcbb79ca98591c2
Will Pride <wpride@dimagi.com>
2016-01-26 14:57:30 -0500

    Go to standalone test instead of integration

--- src/test/java/tests/FormEntryTest.java
+++ src/test/java/tests/FormEntryTest.java
@@ -1,23 +1,30 @@
 package tests;
 
 import application.Application;
+import application.SessionController;
 import auth.HqAuth;
 import beans.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import objects.SerializableSession;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.Result;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.*;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -29,33 +36,42 @@ import org.springframework.web.context.WebApplicationContext;
 import repo.SessionRepo;
 import requests.NewFormRequest;
 import services.XFormService;
+import services.impl.XFormServiceImpl;
 import utils.FileUtils;
+import utils.TestContext;
 
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringApplicationConfiguration(classes = Application.class)   // 2
-@WebAppConfiguration   // 3
-@IntegrationTest("server.port:0")   // 4
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestContext.class)
 public class FormEntryTest {
 
-    @Mock
-    XFormService xFormServiceMock;
-
     private MockMvc mockMvc;
- 
-    @Value("${local.server.port}")   // 6
-    int port;
+
+    @Autowired
+    private SessionRepo sessionRepoMock;
+
+    @Autowired
+    private XFormService xFormServiceMock;
+
+    @InjectMocks
+    private SessionController sessionController;
  
     @Before
     public void setUp() {
-        WebApplicationContext context = (WebApplicationContext) SpringApplication.run(Application.class);
-        mockMvc = MockMvcBuilders.<StandaloneMockMvcBuilder>webAppContextSetup(context).build();
+        Mockito.reset(sessionRepoMock);
+        Mockito.reset(xFormServiceMock);
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(sessionController).build();
     }
 
     public AnswerQuestionResponseBean answerQuestionGetResult(String index, String answer, String sessionId) throws Exception {
@@ -78,8 +94,27 @@ public class FormEntryTest {
     //Integration test of form entry functions
     @Test
     public void testFormEntry() throws Exception {
+
+        final SerializableSession serializableSession =  new SerializableSession();
+
+        when(sessionRepoMock.find(anyString())).thenReturn(serializableSession);
+
+        ArgumentCaptor<SerializableSession> argumentCaptor = ArgumentCaptor.forClass(SerializableSession.class);
+
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Object[] args = invocationOnMock.getArguments();
+                SerializableSession toBeSaved = (SerializableSession) args[0];
+                serializableSession.setInstanceXml(toBeSaved.getInstanceXml());
+                serializableSession.setFormXml(toBeSaved.getFormXml());
+                return null;
+            }
+        }).when(sessionRepoMock).save(Matchers.any(SerializableSession.class));
+
         when(xFormServiceMock.getFormXml(anyString(), any(HqAuth.class)))
                 .thenReturn(FileUtils.getFile(this.getClass(), "xforms/question_types.xml"));
+
         String requestPayload = FileUtils.getFile(this.getClass(), "requests/new_form/new_form_2.json");
 
         ObjectMapper mapper = new ObjectMapper();
@@ -93,6 +128,7 @@ public class FormEntryTest {
         String sessionId = resultJson.getString("session_id");
 
         AnswerQuestionResponseBean response = answerQuestionGetResult("1","William Pride", sessionId);
+
         response = answerQuestionGetResult("2","345", sessionId);
         response = answerQuestionGetResult("3","2.54", sessionId);
         response = answerQuestionGetResult("4","1970-10-23", sessionId);
