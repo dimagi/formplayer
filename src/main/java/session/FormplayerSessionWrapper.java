844a67c9fa690fca2377e8ca305c2caf9976c37e
Will Pride <wpride@dimagi.com>
2016-05-17 17:41:45 -0400

    removed unused

--- src/main/java/session/FormplayerSessionWrapper.java
+++ src/main/java/session/FormplayerSessionWrapper.java
@@ -32,33 +32,4 @@ public class FormplayerSessionWrapper extends SessionWrapper {
 
         return initializer;
     }
-
-    private String getBestTitleHelper() {
-        
-        String[] stepTitles;
-        try {
-            stepTitles = getHeaderTitles();
-        } catch (NoLocalizedTextException e) {
-            // localization resources may not be installed while in the middle
-            // of an update, so default to a generic title
-            return null;
-        }
-
-        Vector<StackFrameStep> v = getFrame().getSteps();
-
-        //So we need to work our way backwards through each "step" we've taken, since our RelativeLayout
-        //displays the Z-Order b insertion (so items added later are always "on top" of items added earlier
-        String bestTitle = null;
-        for (int i = v.size() - 1; i >= 0; i--) {
-            if (bestTitle != null) {
-                break;
-            }
-            StackFrameStep step = v.elementAt(i);
-
-            if (!SessionFrame.STATE_DATUM_VAL.equals(step.getType())) {
-                bestTitle = stepTitles[i];
-            }
-        }
-        return bestTitle;
-    }
 }
