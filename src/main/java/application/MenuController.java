6d249fccc9e0fdb6ebaebc724cd8958fbdcc9121
Will Pride <wpride@dimagi.com>
2016-05-18 19:38:32 -0400

    error message

--- src/main/java/application/MenuController.java
+++ src/main/java/application/MenuController.java
@@ -130,7 +130,7 @@ public class MenuController {
             else if (nextScreen instanceof EntityScreen) {
                 menuResponseBean = generateEntityScreen((EntityScreen) nextScreen);
             } else{
-                throw new Exception("What screen are we on? " + nextScreen);
+                throw new Exception("Unable to recognize next screen: " + nextScreen);
             }
             menuResponseBean.setSessionId(menuSession.getSessionId());
             return menuResponseBean;
