0f1f679f61c58f3b94c049d3bd31f20dd186f4a8
Will Pride <wpride@dimagi.com>
2016-05-18 19:44:28 -0400

    fail hard on XPath exception in case list

--- src/main/java/beans/menus/EntityListResponse.java
+++ src/main/java/beans/menus/EntityListResponse.java
@@ -70,8 +70,7 @@ public class EntityListResponse extends MenuSessionBean {
             try {
                 o = field.getTemplate().evaluate(context);
             } catch(XPathException e) {
-                o = "error (see output)";
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
             data[i] = o;
             i ++;
