50359f14e4926274dec86cc73536fc8c677c5d81
Will Pride <wpride@dimagi.com>
2016-06-23 16:43:08 -0400

    add sessionListItem

--- src/main/java/beans/SessionListItem.java
+++ src/main/java/beans/SessionListItem.java
@@ -1,7 +1,24 @@
 package beans;
 
+import objects.SerializableFormSession;
+
 /**
  * Created by willpride on 6/23/16.
  */
 public class SessionListItem {
+
+    private String sessionId;
+
+
+    public SessionListItem(SerializableFormSession session){
+        this.sessionId = session.getId();
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
 }
