9d54e2715f96092321810b0dc1113c0690d141b2
Will Pride <wpride@dimagi.com>
2016-07-13 16:50:32 -0400

    delete sql migration file, do java migration

---
+++ src/main/java/db/migration/V1__init.java
@@ -0,0 +1,27 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Example of a Java-based migration.
+ */
+public class V1__init implements SpringJdbcMigration {
+
+    @Override
+    public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
+        jdbcTemplate.execute("CREATE TABLE formplayer_sessions (\n" +
+                "    id text NOT NULL,\n" +
+                "    instancexml text,\n" +
+                "    formxml text,\n" +
+                "    restorexml text,\n" +
+                "    username text,\n" +
+                "    initlang text,\n" +
+                "    sequenceid text,\n" +
+                "    domain text,\n" +
+                "    posturl text,\n" +
+                "    sessiondata bytea,\n" +
+                "    CONSTRAINT sessions_pkey PRIMARY KEY (id)\n" +
+                ")");
+    }
+}
