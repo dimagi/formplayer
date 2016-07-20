caca8583765286b7e9f50ac8e6e7a58e1a35a9c7
Will Pride <wpride@dimagi.com>
2016-07-20 16:07:15 -0400

    merge BaseTestClass and BaseMenuTestClass

--- src/test/java/tests/BaseTestClass.java
+++ src/test/java/tests/BaseTestClass.java
@@ -1,11 +1,17 @@
 package tests;
 
 import application.FormController;
+import application.MenuController;
 import application.UtilController;
 import auth.HqAuth;
 import beans.*;
+import beans.menus.CommandListResponseBean;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import install.FormplayerConfigEngine;
 import objects.SerializableFormSession;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.mockito.InjectMocks;
 import org.mockito.Matchers;
@@ -22,6 +28,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import repo.SessionRepo;
+import services.InstallService;
 import services.RestoreService;
 import services.SubmitService;
 import services.XFormService;
@@ -29,7 +36,9 @@ import util.Constants;
 import utils.FileUtils;
 
 import javax.servlet.http.Cookie;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -48,6 +57,8 @@ public class BaseTestClass {
 
     private MockMvc mockUtilController;
 
+    private MockMvc mockMenuController;
+
     @Autowired
     private SessionRepo sessionRepoMock;
 
@@ -60,13 +71,19 @@ public class BaseTestClass {
     @Autowired
     SubmitService submitServiceMock;
 
+    @Autowired
+    private InstallService installService;
+
     @InjectMocks
     protected FormController formController;
 
     @InjectMocks
     protected UtilController utilController;
 
-    private ObjectMapper mapper;
+    @InjectMocks
+    protected MenuController menuController;
+
+    protected ObjectMapper mapper;
 
     final SerializableFormSession serializableFormSession = new SerializableFormSession();
 
@@ -76,15 +93,97 @@ public class BaseTestClass {
         Mockito.reset(xFormServiceMock);
         Mockito.reset(restoreServiceMock);
         Mockito.reset(submitServiceMock);
+        Mockito.reset(installService);
         MockitoAnnotations.initMocks(this);
         mockFormController = MockMvcBuilders.standaloneSetup(formController).build();
         mockUtilController = MockMvcBuilders.standaloneSetup(utilController).build();
+        mockMenuController = MockMvcBuilders.standaloneSetup(menuController).build();
         when(restoreServiceMock.getRestoreXml(anyString(), any(HqAuth.class)))
-                .thenReturn(FileUtils.getFile(this.getClass(), "test_restore_3.xml"));
+                .thenReturn(FileUtils.getFile(this.getClass(), "test_restore.xml"));
         when(submitServiceMock.submitForm(anyString(), anyString(), any(HqAuth.class)))
                 .thenReturn(new ResponseEntity<String>(HttpStatus.OK));
         mapper = new ObjectMapper();
         setUpSessionRepoMock();
+        setupInstallServiceMock();
+    }
+
+    private String resolveAppId(String ref){
+        String appId = ref.substring(ref.indexOf("app_id=") + "app_id=".length(),
+                ref.indexOf("#hack"));
+        switch (appId) {
+            case "doublemgmtappid":
+                ref = "archives/parent_child.ccz";
+                break;
+            case "navigatorappid":
+                ref = "archives/parent_child.ccz";
+                break;
+            case "caseappid":
+                ref = "apps/basic2/profile.ccpr";
+                break;
+            case "createappid":
+                ref = "archives/basic.ccz";
+                break;
+            case "casemediaappid":
+                ref = "archives/casemedia.ccz";
+                break;
+            case "endformappid":
+                ref = "archives/formnav.ccz";
+                break;
+            case "langsappid":
+                ref = "archives/langs.ccz";
+                break;
+            default:
+                throw new RuntimeException("Couldn't resolve appId for ref: " + ref);
+        }
+        return ref;
+    }
+
+    private void setupInstallServiceMock() {
+        try {
+            doAnswer(new Answer<Object>() {
+                @Override
+                public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                    try {
+                        Object[] args = invocationOnMock.getArguments();
+                        String ref = (String) args[0];
+                        if(ref.contains("#hack=commcare.ccz")){
+                            ref = resolveAppId(ref);
+                        }
+                        String username = (String) args[1];
+                        String path = (String) args[2];
+                        FormplayerConfigEngine engine = new FormplayerConfigEngine(username, path);
+                        String absolutePath = getTestResourcePath(ref);
+                        System.out.println("Init with path: " + absolutePath);
+                        if (absolutePath.endsWith(".ccpr")) {
+                            engine.initFromLocalFileResource(absolutePath);
+                        } else if (absolutePath.endsWith(".ccz")) {
+                            engine.initFromArchive(absolutePath);
+                        } else {
+                            throw new RuntimeException("Can't install with reference: " + absolutePath);
+                        }
+                        engine.initEnvironment();
+                        return engine;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        throw e;
+                    }
+                }
+            }).when(installService).configureApplication(anyString(), anyString(), anyString());
+        } catch(Exception e){
+            // don't think we need error handling for mocking
+            e.printStackTrace();
+        }
+    }
+
+    private String getTestResourcePath(String resourcePath){
+        try {
+            URL url = this.getClass().getClassLoader().getResource(resourcePath);
+            File file = new File(url.getPath());
+            return file.getAbsolutePath();
+        } catch(NullPointerException npe){
+            npe.printStackTrace();
+            throw npe;
+        }
     }
 
     private void setUpSessionRepoMock(){
@@ -282,5 +381,52 @@ public class BaseTestClass {
         String evaluateXpathResultString = evaluateXpathResult.andReturn().getResponse().getContentAsString();
         return mapper.readValue(evaluateXpathResultString,
                 EvaluateXPathResponseBean.class);
+    }
+
+    JSONObject sessionNavigate(String requestPath) throws Exception {
+        SessionNavigationBean sessionNavigationBean = mapper.readValue
+                (FileUtils.getFile(this.getClass(), requestPath), SessionNavigationBean.class);
+        ResultActions selectResult = mockMenuController.perform(
+                post(urlPrepend(Constants.URL_MENU_NAVIGATION))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .cookie(new Cookie(Constants.POSTGRES_DJANGO_SESSION_ID, "derp"))
+                        .content(mapper.writeValueAsString(sessionNavigationBean)));
+        String resultString = selectResult.andReturn().getResponse().getContentAsString();
+        return new JSONObject(resultString);
+    }
+
+    JSONObject sessionNavigate(String[] selections, String testName) throws Exception {
+        return sessionNavigate(selections, testName, null);
+    }
+
+    JSONObject sessionNavigate(String[] selections, String testName, String locale) throws Exception {
+        SessionNavigationBean sessionNavigationBean = new SessionNavigationBean();
+        sessionNavigationBean.setDomain(testName + "domain");
+        sessionNavigationBean.setAppId(testName + "appid");
+        sessionNavigationBean.setUsername(testName + "username");
+        sessionNavigationBean.setSelections(selections);
+        if(locale != null && !"".equals(locale.trim())){
+            sessionNavigationBean.setLocale(locale);
+        }
+        ResultActions selectResult = mockMenuController.perform(
+                post(urlPrepend(Constants.URL_MENU_NAVIGATION))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .cookie(new Cookie(Constants.POSTGRES_DJANGO_SESSION_ID, "derp"))
+                        .content(mapper.writeValueAsString(sessionNavigationBean)));
+        String resultString = selectResult.andReturn().getResponse().getContentAsString();
+        return new JSONObject(resultString);
+    }
+
+    CommandListResponseBean doInstall(String requestPath) throws Exception {
+        InstallRequestBean installRequestBean = mapper.readValue
+                (FileUtils.getFile(this.getClass(), requestPath), InstallRequestBean.class);
+        ResultActions installResult = mockMenuController.perform(
+                post(urlPrepend(Constants.URL_INSTALL))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .cookie(new Cookie(Constants.POSTGRES_DJANGO_SESSION_ID, "derp"))
+                        .content(mapper.writeValueAsString(installRequestBean)));
+        String installResultString = installResult.andReturn().getResponse().getContentAsString();
+        return mapper.readValue(installResultString,
+                CommandListResponseBean.class);
     }
 }
