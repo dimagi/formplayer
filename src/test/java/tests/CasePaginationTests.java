d342b95eeb0e11d16c6c5159f29ca8528a0b09c3
Will Pride <wpride@dimagi.com>
2016-07-26 15:02:08 -0400

    merge, fix tests

--- src/test/java/tests/CasePaginationTests.java
+++ src/test/java/tests/CasePaginationTests.java
@@ -39,8 +39,8 @@ public class CasePaginationTests extends BaseMenuTestClass {
         EntityListResponse entityListResponse =
                 mapper.readValue(sessionNavigateResponse.toString(), EntityListResponse.class);
         assert entityListResponse.getEntities().length == EntityListResponse.CASE_LENGTH_LIMIT;
-        assert entityListResponse.getEntities()[2].getData()[0].equals("Amanda");
-        assert entityListResponse.getEntities()[2].getData()[1].equals("27/01/16");
+        assert entityListResponse.getEntities()[4].getData()[0].equals("Amanda");
+        assert entityListResponse.getEntities()[4].getData()[1].equals("27/01/16");
         assert entityListResponse.getCurrentPage() == 0;
         assert entityListResponse.getPageCount() == 8;
         assert entityListResponse.getEntities()[0].getDetails().length == 2;
@@ -50,8 +50,8 @@ public class CasePaginationTests extends BaseMenuTestClass {
         EntityListResponse entityListResponse2 =
                 mapper.readValue(sessionNavigateResponse2.toString(), EntityListResponse.class);
         assert entityListResponse2.getEntities().length == 2;
-        assert entityListResponse2.getEntities()[0].getData()[0].equals("clqrk test");
-        assert entityListResponse2.getEntities()[0].getData()[1].equals("21/04/16");
+        assert entityListResponse2.getEntities()[0].getData()[0].equals("Mycase133");
+        assert entityListResponse2.getEntities()[0].getData()[1].equals("30/04/15");
         assert entityListResponse2.getCurrentPage() == 7;
         assert entityListResponse2.getPageCount() == 8;
 
@@ -62,9 +62,11 @@ public class CasePaginationTests extends BaseMenuTestClass {
         assert firstDetail.getDetails().length == 4;
         assert secondDetail.getDetails().length == 6;
 
-        assert firstDetail.getDetails()[0].equals("clqrk test");
-        assert secondDetail.getDetails()[0].equals("51");
-        assert secondDetail.getDetails()[1].equals("21/04/16");
+        System.out.println("First detail: " + firstDetail + ", second detail: " + secondDetail);
+
+        assert firstDetail.getDetails()[0].equals("Mycase133");
+        assert secondDetail.getDetails()[0].equals("106");
+        assert secondDetail.getDetails()[1].equals("30/04/15");
 
         assert firstDetail.getHeaders()[0].equals("Name");
         assert secondDetail.getHeaders()[2].equals("Intval");
@@ -78,8 +80,8 @@ public class CasePaginationTests extends BaseMenuTestClass {
         EntityListResponse entityListResponse =
                 mapper.readValue(sessionNavigateResponse.toString(), EntityListResponse.class);
         assert entityListResponse.getEntities().length == 9;
-        assert entityListResponse.getEntities()[0].getData()[0].equals("Casetest");
-        assert entityListResponse.getEntities()[1].getData()[0].equals("Test 2");
+        assert entityListResponse.getEntities()[2].getData()[0].equals("Casetest");
+        assert entityListResponse.getEntities()[5].getData()[0].equals("Test 2");
         assert entityListResponse.getCurrentPage() == 0;
         assert entityListResponse.getPageCount() == 0;
     }
@@ -90,11 +92,10 @@ public class CasePaginationTests extends BaseMenuTestClass {
                 sessionNavigate("requests/navigators/search_paginate_navigator.json");
         EntityListResponse entityListResponse =
                 mapper.readValue(sessionNavigateResponse.toString(), EntityListResponse.class);
-
         assert entityListResponse.getEntities().length == 10;
         assert entityListResponse.getPageCount() == 2;
         assert entityListResponse.getCurrentPage() == 1;
-        assert entityListResponse.getEntities()[0].getData()[0].equals("RESTOSE");
-        assert entityListResponse.getEntities()[1].getData()[0].equals("Test 1");
+        assert entityListResponse.getEntities()[0].getData()[0].equals("Christy");
+        assert entityListResponse.getEntities()[1].getData()[0].equals("Test 2");
     }
 }
