11e9d81564ced252d7f95d2a0375b444ac972d6e
Ben <rudolphben@gmail.com>
2016-08-29 22:04:07 -0400

    Add a few docs
    Former-commit-id: 258f9686d3f8281d2a7579a9401221a8f2b313a7

--- src/main/java/exceptions/ApplicationConfigException.java
+++ src/main/java/exceptions/ApplicationConfigException.java
@@ -1,7 +1,9 @@
 package exceptions;
 
 /**
- * Created by benrudolph on 8/29/16.
+ * ApplicationConfigException should be raised when an app config
+ * error occurs such as a bad xpath query. When an ApplicationConfigException
+ * is thrown, no error emails are sent.
  */
 public class ApplicationConfigException extends RuntimeException {
     public ApplicationConfigException(String message) {
