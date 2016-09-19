0dd1b75ef68a84e61586b47c16216792aae03a69
Will Pride <wpride@dimagi.com>
2016-09-19 16:57:45 -0400

    remove unused

--- src/main/java/beans/NewSessionRequestBean.java
+++ src/main/java/beans/NewSessionRequestBean.java
@@ -21,7 +21,6 @@ public class NewSessionRequestBean extends AuthenticatedRequestBean {
     private String instanceContent;
     private String postUrl;
     private boolean oneQuestionPerScreen;
-    private boolean debuggerEnabled;
 
     // our JSON-Object mapping lib (Jackson) requires a default constructor
     public NewSessionRequestBean(){}
