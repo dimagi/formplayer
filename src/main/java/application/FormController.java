60fe268972a2140c9ef2a39912bb9bc6b935192b
Will Pride <wpride@dimagi.com>
2016-09-19 16:50:55 -0400

    attach instance xml to form responses
    Former-commit-id: f0da62732af7fecc9049cdf977efece341f2c76d

--- src/main/java/application/FormController.java
+++ src/main/java/application/FormController.java
@@ -108,6 +108,7 @@ public class FormController extends AbstractBaseController{
             FormEntryResponseBean responseBean = mapper.readValue(resp.toString(), FormEntryResponseBean.class);
             responseBean.setTitle(formEntrySession.getTitle());
             responseBean.setSequenceId(formEntrySession.getSequenceId());
+            responseBean.setInstanceXml(new InstanceXmlBean(formEntrySession));
             log.info("Answer response: " + responseBean);
             return responseBean;
         } finally {
@@ -294,10 +295,11 @@ public class FormController extends AbstractBaseController{
                     formEntrySession.getFormEntryModel(),
                     newRepeatRequestBean.getRepeatIndex());
             updateSession(formEntrySession, serializableFormSession);
-            FormEntryResponseBean repeatResponseBean = mapper.readValue(response.toString(), FormEntryResponseBean.class);
-            repeatResponseBean.setTitle(formEntrySession.getTitle());
-            log.info("New response: " + repeatResponseBean);
-            return repeatResponseBean;
+            FormEntryResponseBean responseBean = mapper.readValue(response.toString(), FormEntryResponseBean.class);
+            responseBean.setTitle(formEntrySession.getTitle());
+            responseBean.setInstanceXml(new InstanceXmlBean(formEntrySession));
+            log.info("New response: " + responseBean);
+            return responseBean;
         } finally {
             lock.unlock();
         }
@@ -315,10 +317,11 @@ public class FormController extends AbstractBaseController{
                     formEntrySession.getFormEntryModel(),
                     deleteRepeatRequestBean.getRepeatIndex(), deleteRepeatRequestBean.getFormIndex());
             updateSession(formEntrySession, serializableFormSession);
-            FormEntryResponseBean repeatResponseBean = mapper.readValue(response.toString(), FormEntryResponseBean.class);
-            repeatResponseBean.setTitle(formEntrySession.getTitle());
-            log.info("Delete repeat response: " + repeatResponseBean);
-            return repeatResponseBean;
+            FormEntryResponseBean responseBean = mapper.readValue(response.toString(), FormEntryResponseBean.class);
+            responseBean.setTitle(formEntrySession.getTitle());
+            responseBean.setInstanceXml(new InstanceXmlBean(formEntrySession));
+            log.info("Delete repeat response: " + responseBean);
+            return responseBean;
         } finally {
             lock.unlock();
         }
