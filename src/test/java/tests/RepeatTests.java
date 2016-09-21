76eb6bce56892dfb76d780951c80c01453c7c62d
Will Pride <wpride@dimagi.com>
2016-09-21 14:56:01 -0400

    remove imports, rmeove unused auth

--- src/test/java/tests/RepeatTests.java
+++ src/test/java/tests/RepeatTests.java
@@ -1,7 +1,8 @@
 package tests;
 
-import auth.HqAuth;
-import beans.*;
+import beans.FormEntryResponseBean;
+import beans.NewFormResponse;
+import beans.QuestionBean;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,10 +11,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import utils.FileUtils;
 import utils.TestContext;
 
-import java.io.IOException;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
 /**
