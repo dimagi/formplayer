0a47e824a578e47b938e0b9de2fbc63135fa71fb
Will Pride <wpride@dimagi.com>
2016-10-25 13:40:45 -0700

    rename file to change version

--- src/main/java/db/migration/V9__menu_session_as_user.java
+++ src/main/java/db/migration/V9__menu_session_as_user.java
@@ -6,7 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 /**
  * Give old incomplete forms a default value
  */
-public class V7__menu_session_as_user implements SpringJdbcMigration {
+public class V9__menu_session_as_user implements SpringJdbcMigration {
     @Override
     public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
         jdbcTemplate.execute("ALTER TABLE menu_sessions " +
