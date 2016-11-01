3e2730de36890751768e8878d9ddf53bee3956ac
Will Pride <wpride@dimagi.com>
2016-11-01 16:52:06 -0700

    fix test

--- src/test/java/tests/LoadOldIncompleteTest.java
+++ src/test/java/tests/LoadOldIncompleteTest.java
@@ -5,20 +5,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import repo.impl.PostgresMigratedFormSessionRepo;
 import util.SessionUtils;
 import utils.FileUtils;
 import utils.TestContext;
 
 /**
- * Tests for FormplayerDateUtils
+ * Test loading the JSON from an old cloudcare sessions
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestContext.class)
 public class LoadOldIncompleteTest {
     @Test
     public void testLoadIncomplete() {
-        String path = FileUtils.getFile(this.getClass(), "old_incomplete_session.json");
-        SerializableFormSession session = SessionUtils.loadSessionFromJson(path);
-        System.out.println("Session: " + session);
+        String payload = FileUtils.getFile(this.getClass(), "old_incomplete_session.json");
+        SerializableFormSession session = PostgresMigratedFormSessionRepo.loadSessionFromJson(payload);
+        assert session.getTitle().equals("Surveys > Survey 2");
+        assert session.getDomain().equals("normal");
+        assert session.getUsername().equals("test");
+
     }
 }
