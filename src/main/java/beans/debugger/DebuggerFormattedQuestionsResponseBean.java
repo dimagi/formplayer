754166e5f8c10b32a57034da8d4b4f7966e793b6
Ben <rudolphben@gmail.com>
2016-11-03 14:28:46 -0400

    formatted_questions route for formplayer

---
+++ src/main/java/beans/debugger/DebuggerFormattedQuestionsResponseBean.java
@@ -0,0 +1,52 @@
+package beans.debugger;
+
+import beans.SessionResponseBean;
+
+/**
+ * Created by benrudolph on 11/3/16.
+ */
+public class DebuggerFormattedQuestionsResponseBean {
+    private String xmlns;
+    private String appId;
+    private String instanceXml;
+    private String formattedQuestions;
+
+    public DebuggerFormattedQuestionsResponseBean(String appId, String xmlns, String instanceXml, String formattedQuestions) {
+        this.xmlns = xmlns;
+        this.appId = appId;
+        this.instanceXml = instanceXml;
+        this.formattedQuestions = formattedQuestions;
+    }
+
+    public String getFormattedQuestions() {
+        return formattedQuestions;
+    }
+
+    public void setFormattedQuestions(String formattedQuestions) {
+        this.formattedQuestions = formattedQuestions;
+    }
+
+    public String getXmlns() {
+        return xmlns;
+    }
+
+    public void setXmlns(String xmlns) {
+        this.xmlns = xmlns;
+    }
+
+    public String getInstanceXml() {
+        return instanceXml;
+    }
+
+    public void setInstanceXml(String instanceXml) {
+        this.instanceXml = instanceXml;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+}
