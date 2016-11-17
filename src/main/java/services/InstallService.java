ebdf03b4da17b802705e30dd1f4c8fe6ad0e5464
Will Pride <wpride@dimagi.com>
2016-11-17 17:18:40 -0500

    fix commcare tests, cleanup
    Former-commit-id: db19a022f5f33027e9fcb21e2b732e90c3cec4cf

--- src/main/java/services/InstallService.java
+++ src/main/java/services/InstallService.java
@@ -1,15 +1,10 @@
 package services;
 
-import org.commcare.resources.model.InstallCancelledException;
-import org.commcare.resources.model.UnresolvedResourceException;
 import org.commcare.util.engine.CommCareConfigEngine;
-import org.javarosa.xml.util.UnfullfilledRequirementsException;
-
-import java.io.IOException;
 
 /**
  * Created by willpride on 2/25/16.
  */
 public interface InstallService {
-    CommCareConfigEngine configureApplication(String reference, String username, String dbPath) throws IOException, InstallCancelledException, UnresolvedResourceException, UnfullfilledRequirementsException;
+    CommCareConfigEngine configureApplication(String reference, String username, String dbPath);
 }
--- src/main/java/services/impl/InstallServiceImpl.java
+++ src/main/java/services/impl/InstallServiceImpl.java
@@ -1,22 +1,16 @@
 package services.impl;
 
-import install.FormplayerConfigEngine;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.commcare.api.persistence.SqliteIndexedStorageUtility;
-import org.commcare.resources.model.InstallCancelledException;
-import org.commcare.resources.model.UnresolvedResourceException;
 import org.commcare.util.engine.CommCareConfigEngine;
 import org.javarosa.core.services.PrototypeManager;
 import org.javarosa.core.services.storage.IStorageIndexedFactory;
 import org.javarosa.core.services.storage.IStorageUtilityIndexed;
-import org.javarosa.core.util.externalizable.PrototypeFactory;
-import org.javarosa.xml.util.UnfullfilledRequirementsException;
 import services.InstallService;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
  * Created by willpride on 2/25/16.
@@ -26,7 +20,7 @@ public class InstallServiceImpl implements InstallService {
     private final Log log = LogFactory.getLog(InstallServiceImpl.class);
 
     @Override
-    public CommCareConfigEngine configureApplication(String reference, final String username, final String dbPath) throws IOException, InstallCancelledException, UnresolvedResourceException, UnfullfilledRequirementsException {
+    public CommCareConfigEngine configureApplication(String reference, final String username, final String dbPath) {
         log.info("Configuring application with reference " + reference + " and dbPath: " + dbPath + ".");
 
         final String trimmedUsername = StringUtils.substringBefore(username, "@");
