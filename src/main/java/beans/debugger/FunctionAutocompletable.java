1399a04a894499d7f57e69cb4b2c3812c9ed6b2f
Will Pride <wpride@dimagi.com>
2016-11-28 20:23:27 -0500

    autocomplete

--- src/main/java/beans/debugger/FunctionAutocompletable.java
+++ src/main/java/beans/debugger/FunctionAutocompletable.java
@@ -6,10 +6,16 @@ import org.javarosa.xpath.expr.XPathFuncExpr;
  * Represents a function that can be used to autocomplete XPath entry.
  */
 public class FunctionAutocompletable extends AutoCompletableItem {
+
+    private String documentation;
+
     public FunctionAutocompletable(Class clazz) {
         super(clazz.getName(), clazz.getName(), "Function");
         try {
             XPathFuncExpr function = (XPathFuncExpr) clazz.newInstance();
+
+            setDocumentation(function.getDocumentation());
+
             int argCount = function.getExpectedArgCount();
             String name = function.getName();
             StringBuilder sb = new StringBuilder(name);
@@ -18,7 +24,7 @@ public class FunctionAutocompletable extends AutoCompletableItem {
                 sb.append("${0}");
             }
             if(argCount > 1) {
-                for(int i = 0; i < argCount - 1; i ++) {
+                for(int i = 1; i < argCount; i ++) {
                     sb.append(", ${");
                     sb.append(i);
                     sb.append("}");
@@ -29,5 +35,13 @@ public class FunctionAutocompletable extends AutoCompletableItem {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String getDocumentation() {
+        return documentation;
+    }
+
+    public void setDocumentation(String documentation) {
+        this.documentation = documentation;
     }
 }
