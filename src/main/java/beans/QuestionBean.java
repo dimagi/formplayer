5d9964411c377a00f642e3e0a38b85d8589e2213
Ben <rudolphben@gmail.com>
2017-01-25 13:39:44 +0200

    Reorganize properties and add docs
    Former-commit-id: e52de1038fe564a0807d4e683b0a45ccf6b7d645

--- src/main/java/beans/QuestionBean.java
+++ src/main/java/beans/QuestionBean.java
@@ -14,26 +14,32 @@ import java.util.HashMap;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class QuestionBean {
     private String caption_audio;
-    private String caption;
-    private String binding;
+    private String caption_video;
     private String caption_image;
+    private String caption_markdown;
+    private String caption;
+
+    private String binding;
     private int required;
     private int relevant;
-    private String help;
     private Object answer;
     private String datatype;
+
+    // Style defines appearance attributes for a question
     private HashMap<String, String> style = new HashMap<>();
-    private String caption_video;
     private String type;
-    private String caption_markdown;
     private String ix;
     private String[] choices;
     private String repeatable;
     private String exists;
     private String addChoice;
     private String header;
-    private String hint;
     private int control;
+
+    // The is the help/hint text defined in the formbuilder advanced section.
+    // Read more about its purpose here: https://confluence.dimagi.com/display/commcarepublic/Advanced+CommCare+Android+Formatting#AdvancedCommCareAndroidFormatting-HintandHelpText
+    private String help;
+    private String hint;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private QuestionBean[] children;
