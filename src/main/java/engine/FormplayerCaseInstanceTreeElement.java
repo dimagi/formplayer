ff14f4f9d6b803003ca9981132988946abdbbb53
Will Pride <wpride@dimagi.com>
2017-01-31 14:39:58 +0200

    update
    Former-commit-id: fc626f66c0879e59a0e0d2d4787a97b503e71fa4

--- src/main/java/engine/FormplayerCaseInstanceTreeElement.java
+++ src/main/java/engine/FormplayerCaseInstanceTreeElement.java
@@ -2,6 +2,7 @@ package engine;
 
 import org.commcare.cases.instance.CaseInstanceTreeElement;
 import org.commcare.cases.model.Case;
+import org.commcare.cases.util.PredicateProfile;
 import org.javarosa.core.model.instance.AbstractTreeElement;
 import org.javarosa.core.services.storage.IStorageUtilityIndexed;
 
@@ -17,29 +18,8 @@ public class FormplayerCaseInstanceTreeElement extends CaseInstanceTreeElement {
     }
 
     @Override
-    protected Vector<Integer> getNextIndexMatch(Vector<String> keys, Vector<Object> values,
+    protected Vector<Integer> getNextIndexMatch(Vector<PredicateProfile> profiles,
                                                 IStorageUtilityIndexed<?> storage) throws IllegalArgumentException {
-        if (keys.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-
-        String firstKey = keys.elementAt(0);
-        if (firstKey.startsWith(Case.INDEX_CASE_INDEX_PRE)) {
-            keys.remove(0);
-            values.remove(0);
-            return getNextIndexMatch(keys, values, storage);
-        }
-
-        String key = keys.elementAt(0);
-        Object o = values.elementAt(0);
-
-        //Get matches if it works
-        Vector<Integer> returnValue = storage.getIDsForValue(key, o);
-
-        //If we processed this, pop it off the queue
-        keys.removeElementAt(0);
-        values.removeElementAt(0);
-
-        return returnValue;
+        throw new IllegalArgumentException();
     }
 }
