66ed9be57420cfe4ad9a857b5654323c555f656e
Will Pride <wpride@dimagi.com>
2017-03-06 17:07:25 +0200

    add EntityStorageCache
    Former-commit-id: b4f26354b9419e5d3a1194787037ae0f540ef711

--- src/main/java/parsers/FormplayerCaseXmlParser.java
+++ src/main/java/parsers/FormplayerCaseXmlParser.java
@@ -1,14 +1,48 @@
 package parsers;
 
+import database.models.EntityStorageCache;
+import database.models.FormplayerCaseIndexTable;
+import org.commcare.cases.model.Case;
 import org.commcare.xml.CaseXmlParser;
 import org.javarosa.core.services.storage.IStorageUtilityIndexed;
 import org.kxml2.io.KXmlParser;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by willpride on 3/6/17.
  */
 public class FormplayerCaseXmlParser extends CaseXmlParser {
-    public FormplayerCaseXmlParser(KXmlParser parser, boolean acceptCreateOverwrites, IStorageUtilityIndexed storage) {
+    private File folder;
+    private final boolean processAttachments = true;
+    private final EntityStorageCache mEntityCache;
+    private final FormplayerCaseIndexTable mCaseIndexTable;
+
+    public FormplayerCaseXmlParser(KXmlParser parser, IStorageUtilityIndexed storage,
+                                EntityStorageCache entityCache, FormplayerCaseIndexTable indexTable) {
+        super(parser, storage);
+        mEntityCache = entityCache;
+        mCaseIndexTable = indexTable;
+    }
+
+    public FormplayerCaseXmlParser(KXmlParser parser, IStorageUtilityIndexed storage) {
+        this(parser, storage, new EntityStorageCache("case"), new FormplayerCaseIndexTable());
+    }
+
+    public FormplayerCaseXmlParser(KXmlParser parser, boolean acceptCreateOverwrites,
+                                IStorageUtilityIndexed<Case> storage) {
         super(parser, acceptCreateOverwrites, storage);
+        mEntityCache = new EntityStorageCache("case");
+        mCaseIndexTable = new FormplayerCaseIndexTable();
+    }
+
+
+    @Override
+    public void commit(Case parsed) throws IOException {
+        super.commit(parsed);
+        mEntityCache.invalidateCache(String.valueOf(parsed.getID()));
+        mCaseIndexTable.clearCaseIndices(parsed);
+        mCaseIndexTable.indexCase(parsed);
     }
 }
