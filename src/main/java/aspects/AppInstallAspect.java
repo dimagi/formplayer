3190008464861d2f246c1f5b42fd624d27db08b2
Will Pride <wpride@dimagi.com>
2017-04-14 14:15:29 +0200

    PR comments, add some logging

--- src/main/java/aspects/AppInstallAspect.java
+++ src/main/java/aspects/AppInstallAspect.java
@@ -1,13 +1,6 @@
 package aspects;
 
-import auth.DjangoAuth;
-import auth.HqAuth;
-import auth.TokenAuth;
-import beans.AuthenticatedRequestBean;
 import beans.InstallRequestBean;
-import beans.SessionNavigationBean;
-import beans.SessionRequestBean;
-import hq.models.PostgresUser;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
@@ -16,24 +9,17 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import repo.impl.PostgresUserRepo;
 import services.FormplayerStorageFactory;
-import services.RestoreFactory;
-import util.UserUtils;
 
 import java.util.Arrays;
 
 /**
- * Aspect to configure the RestoreFactory
+ * Aspect to configure the FormplayerStorageManager
  */
 @Aspect
 public class AppInstallAspect {
 
-    private final Log log = LogFactory.getLog(AppInstallAspect.class);
-
     @Autowired
     protected FormplayerStorageFactory storageFactory;
-
-    @Autowired
-    protected PostgresUserRepo postgresUserRepo;
 
     @Before(value = "@annotation(annotations.AppInstall)")
     public void configureStorageFactory(JoinPoint joinPoint) throws Throwable {
