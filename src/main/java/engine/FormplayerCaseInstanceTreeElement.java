d111d46be38f4ecacbf22f833fb0b6d4d0e6ca14
Will Pride <wpride@dimagi.com>
2017-04-21 11:57:14 +0200

    Print behind flag

--- src/main/java/engine/FormplayerCaseInstanceTreeElement.java
+++ src/main/java/engine/FormplayerCaseInstanceTreeElement.java
@@ -6,6 +6,7 @@ import org.apache.commons.logging.LogFactory;
 import org.commcare.modern.engine.cases.CaseGroupResultCache;
 import org.commcare.modern.engine.cases.CaseIndexQuerySetTransform;
 import org.commcare.modern.engine.cases.query.CaseIndexPrefetchHandler;
+import sandbox.SqlHelper;
 import sandbox.SqliteIndexedStorageUtility;
 import org.commcare.cases.instance.CaseInstanceTreeElement;
 import org.commcare.cases.model.Case;
@@ -83,7 +84,9 @@ public class FormplayerCaseInstanceTreeElement extends CaseInstanceTreeElement i
         }
         long value = System.currentTimeMillis() - timeInMillis;
         log.debug("Case iterate took: " + value + "ms");
-        System.out.println("Case iterate took: " + value + "ms");
+        if (SqlHelper.SQL_DEBUG) {
+            System.out.println("Case iterate took: " + value + "ms");
+        }
     }
 
     @Override
