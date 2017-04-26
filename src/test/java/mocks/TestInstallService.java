68f564ed704cfd63dcad1cb7ec0c30717ef0513e
Will Pride <wpride@dimagi.com>
2017-04-26 14:36:40 +0200

    Test refactor

--- src/test/java/mocks/TestInstallService.java
+++ src/test/java/mocks/TestInstallService.java
@@ -1,12 +1,7 @@
 package mocks;
 
-import engine.FormplayerArchiveFileRoot;
 import engine.FormplayerConfigEngine;
-import installers.FormplayerInstallerFactory;
-import sandbox.SqlSandboxUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import services.FormplayerStorageFactory;
-import services.InstallService;
+import services.impl.InstallServiceImpl;
 
 import java.io.File;
 import java.net.URL;
@@ -14,43 +9,11 @@ import java.net.URL;
 /**
  * Created by willpride on 12/7/16.
  */
-public class TestInstallService implements InstallService {
-
-    @Autowired
-    FormplayerStorageFactory storageFactory;
-
-    @Autowired
-    FormplayerInstallerFactory formplayerInstallerFactory;
-
-    @Autowired
-    FormplayerArchiveFileRoot formplayerArchiveFileRoot;
+public class TestInstallService extends InstallServiceImpl {
 
     @Override
-    public FormplayerConfigEngine configureApplication(String reference) {
-        try {
-            File dbFile = new File(storageFactory.getDatabaseFile());
-            if(dbFile.exists()) {
-                // Try reusing old install, fail quietly
-                try {
-                    FormplayerConfigEngine engine = new FormplayerConfigEngine(storageFactory, formplayerInstallerFactory, formplayerArchiveFileRoot);
-                    engine.initEnvironment();
-                    return engine;
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            SqlSandboxUtils.deleteDatabaseFolder(storageFactory.getDatabaseFile());
-            dbFile.getParentFile().mkdirs();
-            FormplayerConfigEngine engine = new FormplayerConfigEngine(storageFactory,
-                    formplayerInstallerFactory, formplayerArchiveFileRoot);
-            String absolutePath = getTestResourcePath(reference);
-            engine.initFromArchive(absolutePath);
-            engine.initEnvironment();
-            return engine;
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+    public FormplayerConfigEngine configureApplication(String reference) throws Exception {
+        return super.configureApplication(getTestResourcePath(reference));
     }
 
     private String getTestResourcePath(String resourcePath){
