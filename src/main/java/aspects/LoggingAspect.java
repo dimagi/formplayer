7595950a655f88f4ca29f6f4bfb2748e2f1bc2df
Ben <rudolphben@gmail.com>
2017-04-27 18:32:21 +0200

    Add raven to form player. That's _so_ raven
    Former-commit-id: ad44e4f43a0f4a2ff2bc582de5dd89cb31c5257d

--- src/main/java/aspects/LoggingAspect.java
+++ src/main/java/aspects/LoggingAspect.java
@@ -1,14 +1,22 @@
 package aspects;
 
+import beans.AuthenticatedRequestBean;
+import com.getsentry.raven.Raven;
+import com.getsentry.raven.event.BreadcrumbBuilder;
+import com.getsentry.raven.event.Breadcrumbs;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.ShellProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Aspect to log the inputs and return of each API method
@@ -18,6 +26,10 @@ public class LoggingAspect {
 
     private final Log log = LogFactory.getLog(LoggingAspect.class);
 
+    @Autowired
+    protected Raven raven;
+
+
     @Around(value = "@annotation(org.springframework.web.bind.annotation.RequestMapping) " +
             "&& !@annotation(annotations.NoLogging)")
     public Object logRequest(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -25,8 +37,17 @@ public class LoggingAspect {
         Method m = ms.getMethod();
         String requestPath = m.getAnnotation(RequestMapping.class).value()[0]; //Should be only one
         try {
-            Object requestBean = joinPoint.getArgs()[0];
+            AuthenticatedRequestBean requestBean = (AuthenticatedRequestBean) joinPoint.getArgs()[0];
             log.info("Request to " + requestPath + " with bean " + requestBean);
+            BreadcrumbBuilder builder = new BreadcrumbBuilder();
+
+            Map<String, String> data = new HashMap<String, String>();
+            data.put("path", requestPath);
+            data.put("domain", requestBean.getDomain());
+            data.put("username", requestBean.getUsername());
+            data.put("restoreAs", requestBean.getRestoreAs());
+            builder.setData(data);
+            Breadcrumbs.record(builder.build());
         } catch(ArrayIndexOutOfBoundsException e) {
             // no request body
             log.info("Request to " + requestPath + " with no request body.");
