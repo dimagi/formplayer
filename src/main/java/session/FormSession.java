5c87fcc3c038ca9910f2cebf265a7361c7a3c9e2
Will Pride <wpride@dimagi.com>
2017-05-05 17:20:34 +0200

    don't need to update currentIndex anymore

--- src/main/java/session/FormSession.java
+++ src/main/java/session/FormSession.java
@@ -469,15 +469,6 @@ public class FormSession {
         } else {
             initialize(true, sessionData);
         }
-        if (this.oneQuestionPerScreen) {
-            FormIndex firstIndex = JsonActionUtils.indexFromString(currentIndex, this.formDef);
-            IFormElement element = formEntryController.getModel().getForm().getChild(firstIndex);
-            while (element instanceof GroupDef && !formEntryController.isFieldListHost(firstIndex)) {
-                firstIndex =  formController.getNextFormIndex(firstIndex, false, true);
-                element = formEntryController.getModel().getForm().getChild(firstIndex);
-            }
-            this.currentIndex = firstIndex.toString();
-        }
         this.postUrl = postUrl;
     }
 
