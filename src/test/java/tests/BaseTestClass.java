2f570bd682fac840eaed37003d49a2381ad840d7
Will Pride <wpride@dimagi.com>
2017-05-08 15:02:33 +0200

    Re-write FormValidatorTests to use BaseTestClass and mocked context

--- src/test/java/tests/BaseTestClass.java
+++ src/test/java/tests/BaseTestClass.java
@@ -38,6 +38,7 @@ import utils.TestContext;
 import javax.servlet.http.Cookie;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -473,6 +474,18 @@ public class BaseTestClass {
                 CommandListResponseBean.class);
     }
 
+    ResultActions doValidate(String formXML) throws Exception {
+        MediaType contentType = new MediaType(MediaType.APPLICATION_XML.getType(),
+                MediaType.APPLICATION_XML.getSubtype(),
+                Charset.forName("utf8"));
+        return generateMockQuery(ControllerType.UTIL,
+                RequestType.POST,
+                Constants.URL_VALIDATE_FORM,
+                formXML,
+                ResultActions.class,
+                contentType);
+    }
+
     public enum RequestType {
         POST, GET
     }
@@ -486,6 +499,22 @@ public class BaseTestClass {
                                     String urlPath,
                                     Object bean,
                                     Class<T> clazz) throws Exception {
+        return generateMockQuery(controllerType,
+                requestType,
+                urlPath,
+                bean,
+                clazz,
+                MediaType.APPLICATION_JSON
+
+        );
+    }
+
+    private <T> T generateMockQuery(ControllerType controllerType,
+                                    RequestType requestType,
+                                    String urlPath,
+                                    Object bean,
+                                    Class<T> clazz,
+                                    MediaType contentType) throws Exception {
         MockMvc controller = null;
         ResultActions evaluateXpathResult = null;
 
@@ -518,7 +547,7 @@ public class BaseTestClass {
             case POST:
                 evaluateXpathResult = controller.perform(
                         post(urlPrepend(urlPath))
-                                .contentType(MediaType.APPLICATION_JSON)
+                                .contentType(contentType)
                                 .cookie(new Cookie(Constants.POSTGRES_DJANGO_SESSION_ID, "derp"))
                                 .content((String) bean));
                 break;
@@ -526,11 +555,16 @@ public class BaseTestClass {
             case GET:
                 evaluateXpathResult = controller.perform(
                         get(urlPrepend(urlPath))
-                                .contentType(MediaType.APPLICATION_JSON)
+                                .contentType(contentType)
                                 .cookie(new Cookie(Constants.POSTGRES_DJANGO_SESSION_ID, "derp"))
                                 .content((String) bean));
                 break;
         }
+
+        if (clazz == ResultActions.class) {
+            return (T) evaluateXpathResult;
+        }
+
         return mapper.readValue(
                 evaluateXpathResult.andReturn().getResponse().getContentAsString(),
                 clazz
