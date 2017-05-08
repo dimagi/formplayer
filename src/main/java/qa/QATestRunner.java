137f5cdcf4919431d755c2781bea19101245d953
Will Pride <wpride@dimagi.com>
2017-05-08 12:48:14 +0200

    Attempt re-writing QA tests

--- src/main/java/qa/QATestRunner.java
+++ src/main/java/qa/QATestRunner.java
@@ -18,10 +18,8 @@ import org.json.JSONObject;
 import qa.steps.*;
 import util.StringUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
--- src/test/java/tests/QaRunnerTests.java
+++ src/test/java/tests/QaRunnerTests.java
@@ -1,17 +1,25 @@
 package tests;
 
+import beans.RunQARequestBean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import utils.FileUtils;
 import utils.TestContext;
 
 /**
  * Regression tests for fixed behaviors
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestContext.class)
-public class QaRunnerTests extends BaseTestClass{
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class QaRunnerTests extends BaseTestClass {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Override
     public void setUp() throws Exception {
@@ -19,7 +27,13 @@ public class QaRunnerTests extends BaseTestClass{
         configureRestoreFactory("qarunnerdomain", "qarunnerusername");
     }
     @Test
-    public void runBasicQa() throws Exception{
-        runQaPlan("requests/run_qa/basic.json", "qaplans/basic.feature");
+    public void runBasicQa() throws Exception {
+        String testPlan = FileUtils.getFile(this.getClass(), "qaplans/basic.feature");
+        RunQARequestBean requestBean = mapper.readValue(
+                FileUtils.getFile(this.getClass(), "requests/run_qa/basic.json"),
+                RunQARequestBean.class);
+        requestBean.setQaPlan(testPlan);
+        String body = this.restTemplate.getForObject(mapper.writeValueAsString(requestBean),
+                String.class);
     }
 }
