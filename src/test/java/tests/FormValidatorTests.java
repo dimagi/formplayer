9cc63b4b0005007d0cf24e7aa86ea1f91fc24a70
Will Pride <wpride@dimagi.com>
2017-05-08 12:48:04 +0200

    Update plugin versions, update deprecated classes
    # Conflicts:
    #	build.gradle
    
    
    Former-commit-id: c9db099f94de1112c8b9895f444c27603bd0dfa9

--- src/test/java/tests/FormValidatorTests.java
+++ src/test/java/tests/FormValidatorTests.java
@@ -8,11 +8,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -36,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@ContextConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class FormValidatorTests {
 
