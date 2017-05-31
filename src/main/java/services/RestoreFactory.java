52db4eae1d61c6447966cb4f8f26daf1f400c0be
Will Pride <wpride@dimagi.com>
2017-05-31 14:54:16 -0400

    Revert "Don't use sync token anymore"
    This reverts commit 9507288f5971b1b4d625aa5e2e33a0575fbeffbf.

--- src/main/java/services/RestoreFactory.java
+++ src/main/java/services/RestoreFactory.java
@@ -357,12 +357,10 @@ public class RestoreFactory implements ConnectionHandler{
         if (overwriteCache) {
             builder.append("&overwrite_cache=true");
         }
-        /*
         String syncToken = getSyncToken(getWrappedUsername());
         if (syncToken != null) {
             builder.append("&since=").append(syncToken);
         }
-        */
         if( asUsername != null) {
             builder.append("&as=" + asUsername + "@" + domain + ".commcarehq.org");
         }
