1c94dfde7cb5a12694fe789948ed65cb0e45fa5c
Ben Rudolph <rudolphben@gmail.com>
2017-06-05 17:09:39 -0400

    Revert "Incremental Sync Episode II"

--- src/main/java/util/UserUtils.java
+++ src/main/java/util/UserUtils.java
@@ -14,11 +14,4 @@ public class UserUtils {
     public static String anonymousUsername(String domain) {
         return Constants.ANONYMOUS_USERNAME + "@" + domain + "." + Constants.COMMCARE_USER_SUFFIX;
     }
-
-    // If given a domained username, return the un-domained username, otherwise return @param username
-    public static String unwrapUsername(String wrappedUsername) {
-        return wrappedUsername.contains("@")
-                ? wrappedUsername.substring(0, wrappedUsername.indexOf("@"))
-                : wrappedUsername;
-    }
 }
