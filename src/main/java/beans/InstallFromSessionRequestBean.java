0270c8cb652dbcc140c04523486b9f1eaacc6174
Ben <rudolphben@gmail.com>
2017-06-16 11:12:25 -0400

    minor cleanup

--- src/main/java/beans/InstallFromSessionRequestBean.java
+++ src/main/java/beans/InstallFromSessionRequestBean.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonSetter;
 
 /**
- * Created by willpride on 2/4/16.
+ * Created by benrudolph on 2/4/16.
+ *
+ * Use this request bean to make requests that install from a menu session id
+ * rather than by using steps.
  */
 public class InstallFromSessionRequestBean extends AuthenticatedRequestBean {
     protected String menuSessionId;
