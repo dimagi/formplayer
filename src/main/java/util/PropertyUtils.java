385834cffe1e83d6ede529643481e0f6fa609c3f
Will Pride <wpride@dimagi.com>
2017-06-30 12:18:04 -0400

    remove unused prop
    Former-commit-id: 6107caf6379ee0382015ef5ca9b642ae4b7e0a5e

--- src/main/java/util/PropertyUtils.java
+++ src/main/java/util/PropertyUtils.java
@@ -13,7 +13,6 @@ public class PropertyUtils {
 
     public static final String ENABLE_BULK_PERFORMANCE = "cc-enable-bulk-performance";
     public static final String AUTO_PURGE_ENABLED = "cc-auto-purge";
-    public static final String ENABLE_LIVE_QUERY = "cc-enable-live-query";
 
     private static boolean doesPropertyMatch(String key, String defaultValue, String matchingValue) {
         try {
@@ -22,11 +21,6 @@ public class PropertyUtils {
         } catch (RuntimeException e) {
             return defaultValue.equals(matchingValue);
         }
-    }
-
-
-    public static boolean isLiveQueryEnabled() {
-        return doesPropertyMatch(ENABLE_LIVE_QUERY, NO, YES);
     }
 
     public static boolean isBulkPerformanceEnabled() {
