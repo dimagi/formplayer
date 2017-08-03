5072696458d7f23f718a2c8ea0008568a82d6eea
Daniel Roberts <droberts@dimagi.com>
2017-08-03 15:15:43 -0400

    move connection code to DBPathConnectionHandler
    from ApplicationDBPath, UserDBPath, FromplayerStorageFactory, and RestoreFactory
    getting rid of some duplication
    
    
    Former-commit-id: 9481caf550e4fca588c7b32cae2fa1759846a527

--- src/main/java/dbpath/DBPath.java
+++ src/main/java/dbpath/DBPath.java
@@ -1,14 +1,7 @@
 package dbpath;
 
-import org.sqlite.SQLiteConnection;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public interface DBPath {
-    Connection getConnection() throws SQLException;
     String getDatabasePath();
     String getDatabaseName();
     String getDatabaseFile();
-    Boolean matchesConnection(SQLiteConnection sqLiteConnection);
 }
