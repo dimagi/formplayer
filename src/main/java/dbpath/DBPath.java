f34560299adab884b8edaf3e155cbfa6d10e20e1
Daniel Roberts <droberts@dimagi.com>
2017-08-03 12:29:18 -0400

    add getDatabaseName method to DBPath

--- src/main/java/dbpath/DBPath.java
+++ src/main/java/dbpath/DBPath.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 public interface DBPath {
     Connection getConnection() throws SQLException;
     String getDatabasePath();
+    String getDatabaseName();
     String getDatabaseFile();
     Boolean matchesConnection(SQLiteConnection sqLiteConnection);
 }
