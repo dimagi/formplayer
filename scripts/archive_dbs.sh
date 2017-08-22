905a5ff52947a0b373278ce8198fd33820c5e322
Daniel Roberts <droberts@dimagi.com>
2017-08-22 17:32:25 -0400

    add script to create gzip archives from old dbs
    Former-commit-id: 70ec7b0277dddf2e9a9dc8f648aadc5cf5279928

---
+++ scripts/archive_dbs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# example usage
+# ./scripts/archive_dbs.sh dbs +2d
+# to archive all dbs in the dbs/ directory that haven't been accessed for more than 2 days
+
+
+dir=${1}
+atime=${2}
+
+if [ $(echo $atime | cut -c1) != "+" ]
+then
+    echo >&2 "atime must start with +: ${atime}"
+    exit 1
+fi
+
+test -d ${dir} || {
+    echo >&2 "dir must be a directory: ${dir}"
+    exit 1
+}
+
+while read line
+do
+    db="${line}"
+    lock="${line}.lock"
+    gz="${line}.gz"
+
+    touch ${lock}
+    gzip ${db}
+    rm ${lock}
+done < <(find ${dir} -name '*.db' -type f -atime ${atime})
