4d7712e82d271507cf7031c7af50b107fa8562e1
Daniel Roberts <droberts@dimagi.com>
2017-09-01 13:17:26 -0400

    correct timings on bucket boundaries
    5.5s now falls into lt_020s instead of lt_005s, etc.
    
    
    Former-commit-id: 5200cd7dce8028208f58a4d6c4bcccfcf158f685

--- src/main/java/aspects/MetricsAspect.java
+++ src/main/java/aspects/MetricsAspect.java
@@ -62,13 +62,13 @@ public class MetricsAspect {
         long timeInS = timeInMs / 1000;
         if (timeInS < 1) {
             return "lt_001s";
-        } else if (timeInS <= 5) {
+        } else if (timeInS < 5) {
             return "lt_005s";
-        } else if (timeInS <= 20) {
+        } else if (timeInS < 20) {
             return "lt_020s";
-        } else if (timeInS <= 60) {
+        } else if (timeInS < 60) {
             return "lt_060s";
-        } else if (timeInS <= 120) {
+        } else if (timeInS < 120) {
             return "lt_120s";
         } else {
             return "over_120s";
