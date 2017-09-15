b520f27f7ca9892eb765622878148c9243cd2bcd
Daniel Roberts <droberts@dimagi.com>
2017-09-15 14:13:27 -0400

    add /navigate_menu_start as alias to /navigate_menu
    web apps will use the new endpoint when entering an app and the main one for everything else.
    this will help us break out the first click, which often includes lots of app installs,
    from all the other menu navigation
    
    
    Former-commit-id: 85ada0a4d91bdb6cee357b9367dcb26a7942819d

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -25,6 +25,7 @@ public class Constants {
     public final static String URL_GET_SESSION = "get_session";
     public static final String URL_INSTALL = "install";
     public static final String URL_UPDATE = "update";
+    public static final String URL_INITIAL_MENU_NAVIGATION = "navigate_menu_start";
     public static final String URL_MENU_NAVIGATION = "navigate_menu";
     public static final String URL_GET_DETAILS = "get_details";
     public static final String URL_GET_SESSIONS = "get_sessions";
