c415bbb125df7eaa70b90d29415a17d520929a70
Will Pride <wpride@dimagi.com>
2017-11-08 18:37:59 -0500

    Locks for accessing lock array
    Former-commit-id: 9f0296ca997b5db605c2be5bb7eec422879ed1c3

--- src/main/java/services/FormplayerLockRegistry.java
+++ src/main/java/services/FormplayerLockRegistry.java
@@ -17,6 +17,9 @@ import java.util.concurrent.locks.ReentrantLock;
 public class FormplayerLockRegistry implements LockRegistry {
 
     private final FormplayerReentrantLock[] lockTable;
+    // Lock objects for accessing the FormplayerReentrantLock table above
+    // These objects do not change
+    private final Object[] lockTableLocks;
 
     private final int mask;
 
@@ -32,8 +35,10 @@ public class FormplayerLockRegistry implements LockRegistry {
         this.mask = mask;
         int arraySize = this.mask + 1;
         this.lockTable = new FormplayerReentrantLock[arraySize];
+        this.lockTableLocks = new Object[arraySize];
         for (int i = 0; i < arraySize; i++) {
             this.lockTable[i] = new FormplayerReentrantLock();
+            this.lockTableLocks[i] = new Object();
         }
     }
 
@@ -51,19 +56,31 @@ public class FormplayerLockRegistry implements LockRegistry {
     public FormplayerReentrantLock obtain(Object lockKey) {
         Assert.notNull(lockKey, "'lockKey' must not be null");
         Integer lockIndex = lockKey.hashCode() & this.mask;
-        FormplayerReentrantLock lock = this.lockTable[lockIndex];
 
-        Thread ownerThread = lock.getOwner();
+        synchronized (this.lockTableLocks[lockIndex]) {
+            FormplayerReentrantLock lock = this.lockTable[lockIndex];
 
-        if (!threadLives(ownerThread)) {
-            log.error(String.format("Evicted dead thread %s owning lockkey %s.", ownerThread, lockKey));
-            lock = setNewLock(lockIndex);
+            Thread ownerThread = lock.getOwner();
+
+            if (!threadLives(ownerThread)) {
+                log.error(String.format("Evicted dead thread %s owning lockkey %s.", ownerThread, lockKey));
+                lock = setNewLock(lockIndex);
+            }
+            if (lock.isExpired()) {
+                log.error(String.format("Evicting thread %s owning expired lock with lock key %s.", ownerThread, lockKey));
+                ownerThread.interrupt();
+                try {
+                    ownerThread.join(5000);
+                } catch (InterruptedException e) {
+                    // TODO WSP can I ignore this?
+                    throw new RuntimeException(e);
+                }
+                ownerThread.stop();
+                log.error(String.format("Evicted thread %s owning expired lock with lock key %s.", ownerThread, lockKey));
+                lock = setNewLock(lockIndex);
+            }
+            return lock;
         }
-        if (lock.isExpired()) {
-            log.error(String.format("Evicted thread %s owning expired lock with lockkey %s.", ownerThread, lockKey));
-            lock = setNewLock(lockIndex);
-        }
-        return lock;
     }
 
     public class FormplayerReentrantLock extends ReentrantLock {
