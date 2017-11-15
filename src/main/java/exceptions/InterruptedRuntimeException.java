ff480fbdd347ace7ddbbf36133194101dc48f155
Will Pride <wpride@dimagi.com>
2017-11-15 10:31:43 -0500

    Fix error message, class comment
    Former-commit-id: 7defe6a46dc2cec21ad43affb16cac2a6c7d8e79

--- src/main/java/exceptions/InterruptedRuntimeException.java
+++ src/main/java/exceptions/InterruptedRuntimeException.java
@@ -1,7 +1,7 @@
 package exceptions;
 
 /**
- * Created by willpride on 10/5/17.
+ * A RuntimeException to wrap InterruptedException, thrown when Thread is interrupted while sleeping or waiting
  */
 public class InterruptedRuntimeException extends RuntimeException {
     public InterruptedRuntimeException(InterruptedException e) {
