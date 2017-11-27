ef7573cca524257f40be4095d0f1ebedde466cec
Will Pride <wpride@dimagi.com>
2017-11-27 13:43:19 -0500

    Revert "Rename raven -> sentry"
    This reverts commit 4433a8364cdd4f90f29e4e83540d7ccf00e887a3 [formerly 24216e7c77effe8dfe03db1d76521f07c0c6f371].
    
    
    Former-commit-id: 79c9a8be877cf400c2de21ffd2225ffba34f5403

--- src/main/java/services/FormplayerLockRegistry.java
+++ src/main/java/services/FormplayerLockRegistry.java
@@ -11,6 +11,7 @@ import org.springframework.util.Assert;
 import util.Constants;
 import util.FormplayerSentry;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -84,7 +85,7 @@ public class FormplayerLockRegistry implements LockRegistry {
                     log.error(String.format("Unable to evict thread %s owning expired lock with lock key %s.", ownerThread, lockKey));
                     Exception e = new Exception("Unable to get expired lock, owner thread has stack trace");
                     e.setStackTrace(ownerThread.getStackTrace());
-                    raven.sendSentryException(new Exception(e), Event.Level.WARNING);
+                    raven.sendRavenException(new Exception(e), Event.Level.WARNING);
                 }
             }
             return lock;
