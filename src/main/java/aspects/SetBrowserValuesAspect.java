f7421b120cd7f668a6089eb1e2392c1b39eaab83
ALIZA STONE <amstone326@gmail.com>
2018-01-09 12:31:45 -0500

    get rid of source concept and just use provider

--- src/main/java/aspects/SetBrowserValuesAspect.java
+++ src/main/java/aspects/SetBrowserValuesAspect.java
@@ -9,7 +9,6 @@ import org.javarosa.core.model.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import services.BrowserValuesProvider;
-import services.FormplayerTimezoneSource;
 
 /**
  * Created by amstone326 on 1/8/18.
@@ -19,24 +18,20 @@ import services.FormplayerTimezoneSource;
 public class SetBrowserValuesAspect {
 
     @Autowired
-    private FormplayerTimezoneSource timezoneSource;
-
-    @Autowired
     private BrowserValuesProvider browserValuesProvider;
 
     @Before(value = "@annotation(org.springframework.web.bind.annotation.RequestMapping)")
     public void setValues(JoinPoint joinPoint) throws Throwable {
-        DateUtils.setTimezoneProviderSource(timezoneSource);
-
         Object beanArg = joinPoint.getArgs()[0];
-        if (beanArg instanceof AuthenticatedRequestBean) {
-            System.out.println("Setting tz offset in BrowserValuesProvider");
-            browserValuesProvider.setTimezoneOffset((AuthenticatedRequestBean)beanArg);
-        }
         if (beanArg instanceof SessionNavigationBean) {
             System.out.println("Setting location in BrowserValuesProvider");
             browserValuesProvider.setLocation((SessionNavigationBean)beanArg);
         }
+        if (beanArg instanceof AuthenticatedRequestBean) {
+            System.out.println("Setting tz offset in BrowserValuesProvider");
+            browserValuesProvider.setTimezoneOffset((AuthenticatedRequestBean)beanArg);
+        }
+        DateUtils.setTimezoneProvider(browserValuesProvider);
     }
 
 }
