ca5752f7e9478b8f12b17587f0098b50a0f2fcd7
Will Pride <wpride@dimagi.com>
2018-01-30 15:28:48 -0500

    fix formatting

--- src/main/java/beans/FormEntryNavigationResponseBean.java
+++ src/main/java/beans/FormEntryNavigationResponseBean.java
@@ -1,25 +1,31 @@
 package beans;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * Created by biyeun on 10/3/16.
- *
+ * <p>
  * Use this to respond to requests related to in form navigation (next / previous / etc)
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FormEntryNavigationResponseBean extends FormEntryResponseBean {
     private boolean isAtLastIndex = false;
     private boolean isAtFirstIndex = false;
     private String currentIndex;
 
-    public boolean getIsAtLastIndex() { return isAtLastIndex; }
+    public boolean getIsAtLastIndex() {
+        return isAtLastIndex;
+    }
 
     public void setIsAtLastIndex(boolean isAtLastIndex) {
         this.isAtLastIndex = isAtLastIndex;
     }
 
-    public String getCurrentIndex() { return currentIndex; }
+    public String getCurrentIndex() {
+        return currentIndex;
+    }
 
     public void setCurrentIndex(String currentIndex) {
         this.currentIndex = currentIndex;
