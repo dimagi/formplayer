0f3975a261aac9820bf91c47f82181e71f111cc7
ctsims <csims@dimagi.com>
2019-03-15 10:02:20 +0530

    Update UserDB Version to reflect ExternalID change

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -48,7 +48,7 @@ public class Constants {
 
     // Change this version when a backwards incompatible change is made to the
     // mobile sqlite dbs.
-    public static final String SQLITE_DB_VERSION = "V4";
+    public static final String SQLITE_DB_VERSION = "V5";
 
     //Menus
     public static final String MENU_MODULE = "modules";
