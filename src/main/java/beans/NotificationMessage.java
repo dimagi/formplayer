3da33041d73c5e7561401c33a9e20861810c2329
Clayton Sims <csims@dimagi.com>
2020-04-22 02:14:10 -0400

    UX cleanups for volatility warnings

--- src/main/java/beans/NotificationMessage.java
+++ src/main/java/beans/NotificationMessage.java
@@ -4,7 +4,15 @@ package beans;
  * Created by willpride on 1/12/16.
  */
 public class NotificationMessage {
+
+    public enum Type {
+        success,
+        warning,
+        error
+    }
+
     private boolean isError;
+    private String type;
     private String message;
 
     public NotificationMessage(){
@@ -14,6 +22,17 @@ public class NotificationMessage {
     public NotificationMessage(String message, boolean isError) {
         this.message = message;
         this.isError = isError;
+        if(this.isError){
+            this.type = Type.error.name();
+        } else {
+            this.type = Type.success.name();
+        }
+    }
+
+    public NotificationMessage(String message, Type type) {
+        this.message = message;
+        this.type = type.name();
+        this.isError = type == Type.error;
     }
 
     public String getMessage() {
@@ -32,8 +51,16 @@ public class NotificationMessage {
         isError = error;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     @Override
     public String toString() {
-        return String.format("NotificationMessage message=%s, isError=%s", message, isError);
+        return String.format("NotificationMessage message=%s, isError=%s, type=%s", message, isError, type);
     }
 }
