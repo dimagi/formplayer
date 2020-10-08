3a1b5b1d45b4c469b87e53ed335f95fd00b9d603
Kevin Nguyen <knguyen@dimagi.com>
2020-10-08 18:23:21 -0400

    pass through browser timezone

--- src/main/java/org/commcare/formplayer/services/BrowserValuesProvider.java
+++ src/main/java/org/commcare/formplayer/services/BrowserValuesProvider.java
@@ -2,6 +2,8 @@ package org.commcare.formplayer.services;
 
 import org.commcare.formplayer.beans.AuthenticatedRequestBean;
 import org.javarosa.core.model.utils.TimezoneProvider;
+import java.util.TimeZone;
+import java.util.Calendar;
 
 /**
  * Created by amstone326 on 1/8/18.
@@ -9,14 +11,26 @@ import org.javarosa.core.model.utils.TimezoneProvider;
 public class BrowserValuesProvider extends TimezoneProvider {
 
     private int timezoneOffsetMillis = -1;
+    private TimeZone timezoneFromBrowser = null;
 
     public void setTimezoneOffset(AuthenticatedRequestBean bean) {
+        try {
+            timezoneFromBrowser = TimeZone.getTimeZone(bean.getTzFromBrowser());
+        } catch (Exception e) {
+            // TODO: handle and make more specific.
+        }
+
         timezoneOffsetMillis = bean.getTzOffset();
     }
 
     @Override
     public int getTimezoneOffsetMillis() {
         return timezoneOffsetMillis;
+    }
+
+    @Override
+    public TimeZone getTimezone() {
+        return timezoneFromBrowser;
     }
 
 }
