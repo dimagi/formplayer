633aa9276752c003e8a4b792303a580751eb8c73
Simon Kelly <skelly@dimagi.com>
2021-01-13 12:26:44 +0200

    log name of lock

--- src/main/java/org/commcare/formplayer/db/callbacks/LockLoggingCallback.java
+++ src/main/java/org/commcare/formplayer/db/callbacks/LockLoggingCallback.java
@@ -21,6 +21,15 @@ public class LockLoggingCallback implements Callback {
 
     private final Log log = LogFactory.getLog(LockLoggingCallback.class);
 
+    // Copied from org.flywaydb.core.internal.database.postgresql.PostgreSQLAdvisoryLockTemplate
+    private static final long LOCK_MAGIC_NUM =
+            (0x46L << 40) // F
+                    + (0x6CL << 32) // l
+                    + (0x79L << 24) // y
+                    + (0x77 << 16) // w
+                    + (0x61 << 8) // a
+                    + 0x79; // y
+
     @Override
     public boolean supports(Event event, Context context) {
         return event.equals(Event.BEFORE_MIGRATE);
@@ -39,6 +48,10 @@ public class LockLoggingCallback implements Callback {
 
     @Override
     public void handle(Event event, Context context) {
+        String tableName = String.format("\"public\".\"%s\"", context.getConfiguration().getTable());
+        long lockName = LOCK_MAGIC_NUM + tableName.hashCode();
+        this.log(event, String.format("Lock name for this process: %s", lockName));
+
         JdbcTemplate jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true));
         List<Map<String, Object>> locks = jdbcTemplate.queryForList(String.format(
                 "SELECT * FROM pg_locks WHERE locktype = 'advisory'"
