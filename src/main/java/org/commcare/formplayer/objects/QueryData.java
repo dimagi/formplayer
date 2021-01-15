debbdc24fd21ad9840a8838b345481fecb5019fc
Jennifer Schweers <orange.jenny@gmail.com>
2021-01-15 09:50:36 -0500

    Updated QueryData to use command as top-level key

--- src/main/java/org/commcare/formplayer/objects/QueryData.java
+++ src/main/java/org/commcare/formplayer/objects/QueryData.java
@@ -3,33 +3,49 @@ package org.commcare.formplayer.objects;
 import com.fasterxml.jackson.annotation.*;
 
 import java.util.Hashtable;
+import java.util.Map;
+
 
 /**
  * Created by jschweers on 12/28/20.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class QueryData {
+    private String KEY_EXECUTE = "execute";
+    private String KEY_INPUTS = "inputs";
 
-    private Hashtable<String, Hashtable<String, String>> inputs = new Hashtable<>();
-    private Hashtable<String, Boolean> execute = new Hashtable<>();
+    private Hashtable<String, Map<String, Object>> data = new Hashtable<>();
 
-    @JsonGetter(value="inputs")
-    public Hashtable<String, Hashtable<String, String>> getInputs() {
-        return inputs;
+    @JsonGetter(value="data")
+    public Hashtable<String, Map<String, Object>> getData() {
+        return data;
     }
 
-    @JsonSetter(value="inputs")
-    public void setData(Hashtable<String, Hashtable<String, String>> inputs) {
-        this.inputs = inputs;
+    @JsonSetter(value="data")
+    public void setData(Hashtable<String, Map<String, Object>> data) {
+        this.data = data;
     }
 
-    @JsonGetter(value="execute")
-    public Hashtable<String, Boolean> getExecute() {
-        return execute;
-    }
+    public Boolean getExecute(String key) {
+        Map<String, Object> value = this.data.get(key);
+        if (value != null) {
+            return (Boolean) value.get(this.KEY_EXECUTE);
+        }
+        return new Boolean(false);
+    };
 
-    @JsonSetter(value="execute")
-    public void setExecute(Hashtable<String, Boolean> execute) {
-        this.execute = execute;
-    }
+    public Hashtable<String, String> getInputs(String key) {
+        Map<String, Object> value = this.data.get(key);
+        if (value != null) {
+            Map<String, String> valueInputs = (Map<String, String>) value.get(this.KEY_INPUTS);
+            if (valueInputs != null) {
+                Hashtable<String, String> inputs = new Hashtable<String, String>();
+                for (String inputKey : valueInputs.keySet()) {
+                    inputs.put(inputKey, (String) valueInputs.get(inputKey));
+                }
+                return inputs;
+            }
+        }
+        return null;
+    };
 }
