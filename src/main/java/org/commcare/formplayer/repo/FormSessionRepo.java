e7d3bca922567f596fe6db5ff3decf8bea17c84b
Simon Kelly <skelly@dimagi.com>
2021-02-04 14:45:40 +0200

    switch to JPA repo

--- src/main/java/org/commcare/formplayer/repo/FormSessionRepo.java
+++ src/main/java/org/commcare/formplayer/repo/FormSessionRepo.java
@@ -1,13 +1,13 @@
 package org.commcare.formplayer.repo;
 
 import org.commcare.formplayer.objects.SerializableFormSession;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface FormSessionRepo extends CrudRepository<SerializableFormSession, String> {
+public interface FormSessionRepo extends JpaRepository<SerializableFormSession, String> {
     @Query(
         value = "SELECT * FROM formplayer_sessions WHERE username = :username " +
                 "ORDER BY dateopened\\:\\:timestamptz DESC",
