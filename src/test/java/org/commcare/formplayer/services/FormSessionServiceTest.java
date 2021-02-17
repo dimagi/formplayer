524cd6ad037bb3582323305db784daf3bdb8d023
Simon Kelly <skelly@dimagi.com>
2021-02-17 11:26:40 +0200

    Revert "only update changing fields when saving form sessions"

--- src/test/java/org/commcare/formplayer/services/FormSessionServiceTest.java
+++ src/test/java/org/commcare/formplayer/services/FormSessionServiceTest.java
@@ -84,23 +84,23 @@ public class FormSessionServiceTest {
 
         // save a session
         SerializableFormSession session = new SerializableFormSession(sessionId);
-        session.incrementSequence();
+        session.setSequenceId(1);
         formSessionService.saveSession(session);
 
         // cache is populated on save
-        assertEquals(0, getCachedSession(sessionId).get().getSequenceId());
+        assertEquals(1, getCachedSession(sessionId).get().getSequenceId());
 
         // get session hits the cache (repo is mocked)
         session = formSessionService.getSessionById(sessionId);
-        assertEquals(0, session.getSequenceId());
+        assertEquals(1, session.getSequenceId());
 
         // update session
-        session.incrementSequence();
+        session.setSequenceId(2);
         formSessionService.saveSession(session);
 
         // cache and find return updated session
-        assertEquals(1, getCachedSession(sessionId).get().getSequenceId());
-        assertEquals(1, formSessionService.getSessionById(sessionId).getSequenceId());
+        assertEquals(2, getCachedSession(sessionId).get().getSequenceId());
+        assertEquals(2, formSessionService.getSessionById(sessionId).getSequenceId());
     }
 
     @Test
