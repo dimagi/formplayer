696fd42ab75178ae6f4e733b8f1ae316fd0ba363
Simon Kelly <skelly@dimagi.com>
2021-03-04 17:18:35 +0200

    safer docker login

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -4,7 +4,7 @@ DOCKER_IMAGE="dimagi/formplayer"
 TARGET_IMAGE_LATEST="${DOCKER_IMAGE}:latest"
 docker build -t $DOCKER_IMAGE .
 
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
 
 docker tag ${DOCKER_IMAGE} ${TARGET_IMAGE_LATEST}
 docker push ${TARGET_IMAGE_LATEST}
