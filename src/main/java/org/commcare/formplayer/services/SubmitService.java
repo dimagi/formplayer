ff199eb215c8339277f1d15cf41c259f4a25cbe3
Simon Kelly <skelly@dimagi.com>
2021-03-05 13:42:42 +0200

    cleanup SubmitService

--- src/main/java/org/commcare/formplayer/services/SubmitService.java
+++ src/main/java/org/commcare/formplayer/services/SubmitService.java
@@ -1,22 +1,12 @@
 package org.commcare.formplayer.services;
 
 import lombok.extern.apachecommons.CommonsLog;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.commcare.formplayer.util.Constants;
 import org.commcare.formplayer.util.SimpleTimer;
 import org.commcare.formplayer.web.client.WebClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.annotation.RequestScope;
-
-import java.io.IOException;
 
 /**
  * Service that handles form submission to CommCareHQ
@@ -24,7 +14,7 @@ import java.io.IOException;
 @Service
 @RequestScope
 @CommonsLog
-public class SubmitService extends DefaultResponseErrorHandler {
+public class SubmitService {
 
     @Autowired
     RestoreFactory restoreFactory;
@@ -49,13 +39,5 @@ public class SubmitService extends DefaultResponseErrorHandler {
 
     public SimpleTimer getSubmitTimer() {
         return submitTimer;
-    }
-
-    // Overriding the default error handler allows us to perform error handling in FormController
-    // rather than at the Spring level
-    // CS: I am fairly sure this code does nothing and can be removed
-    @Override
-    public void handleError(ClientHttpResponse response) throws IOException {
-        log.error("Error submitting form: " + response);
     }
 }
