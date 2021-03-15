c22538dcb2ad18a233d08b8a6a9783b633cbddc5
Simon Kelly <skelly@dimagi.com>
2021-03-15 12:22:22 +0200

    add filter to wrap request for multiple reads

---
+++ src/main/java/org/commcare/formplayer/request/MultipleReadRequestWrappingFilter.java
@@ -0,0 +1,28 @@
+package org.commcare.formplayer.request;
+
+import org.commcare.formplayer.util.MultipleReadHttpRequest;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Wrap all requests in this request wrapper to allow us to read the body multiple times.
+ * This is necessary since some of the authentication details are contained in the body.
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class MultipleReadRequestWrappingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(new MultipleReadHttpRequest(req), response);
+    }
+
+}
