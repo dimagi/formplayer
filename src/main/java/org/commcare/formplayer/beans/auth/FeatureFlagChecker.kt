56206a1b92ab60ee5a1d46681304720ce29f9670
Shubham Goyal <shubham1g5@gmail.com>
2021-05-20 11:28:44 +0530

    Use static methods

--- src/main/java/org/commcare/formplayer/beans/auth/FeatureFlagChecker.kt
+++ src/main/java/org/commcare/formplayer/beans/auth/FeatureFlagChecker.kt
@@ -1,24 +1,36 @@
 package org.commcare.formplayer.beans.auth
 
+import org.commcare.formplayer.util.RequestUtils
+
 /**
  * Utility class to check for enabled previews and toggles
  */
-object FeatureFlagChecker {
+class FeatureFlagChecker {
 
-    fun isPreviewEnabled(preview: String, userDetailsBean: HqUserDetailsBean): Boolean {
-        return isGranted(HqUserDetailsBean.PREVIEW_PREFIX + preview, userDetailsBean)
-    }
+    companion object {
 
-    fun isToggleEnabled(preview: String, userDetailsBean: HqUserDetailsBean): Boolean {
-        return isGranted(HqUserDetailsBean.TOGGLE_PREFIX + preview, userDetailsBean)
-    }
-
-    private fun isGranted(role: String, userDetailsBean: HqUserDetailsBean): Boolean {
-        for (grantedAuthority in userDetailsBean.authorities) {
-            if(grantedAuthority.authority.contentEquals(role)){
-                return true
-            }
+        @JvmStatic
+        fun isPreviewEnabled(preview: String): Boolean {
+            return isGranted(HqUserDetailsBean.PREVIEW_PREFIX + preview)
         }
-        return false
+
+        @JvmStatic
+        fun isToggleEnabled(toggle: String): Boolean {
+            return isGranted(HqUserDetailsBean.TOGGLE_PREFIX + toggle)
+        }
+
+        private fun isGranted(role: String): Boolean {
+            val userDetailsOptional = RequestUtils.getUserDetails()
+            if (userDetailsOptional.isPresent) {
+                return isGranted(role, userDetailsOptional.get())
+            }
+            return false
+        }
+
+        private fun isGranted(role: String, userDetails: HqUserDetailsBean): Boolean {
+            return userDetails.authorities.firstOrNull { grantedAuthority -> grantedAuthority.authority.contentEquals(role) } != null
+        }
     }
 }
+
+
