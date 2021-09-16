f2a20c64132f6d9a84c0aa7e36c179b704599f6a
Graham Herceg <g.a.herceg@gmail.com>
2021-09-15 18:00:40 -0700

    improved testing around expectations of instance serialization

--- src/test/java/org/commcare/formplayer/tests/SavedFormDefTest.java
+++ src/test/java/org/commcare/formplayer/tests/SavedFormDefTest.java
@@ -2,7 +2,9 @@ package org.commcare.formplayer.tests;
 
 import org.apache.commons.io.IOUtils;
 import org.commcare.formplayer.beans.NewFormResponse;
+import org.commcare.formplayer.beans.SubmitResponseBean;
 import org.commcare.formplayer.objects.SerializableFormSession;
+import org.commcare.formplayer.session.FormSession;
 import org.commcare.formplayer.util.serializer.FormDefStringSerializer;
 import org.commcare.formplayer.utils.FileUtils;
 import org.commcare.formplayer.utils.TestContext;
@@ -10,6 +12,7 @@ import org.javarosa.core.model.FormDef;
 import org.javarosa.model.xform.XFormSerializingVisitor;
 import org.javarosa.xform.util.XFormUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -21,18 +24,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ContextConfiguration(classes = TestContext.class)
 public class SavedFormDefTest extends BaseTestClass {
 
-
     @Test
-    public void testFormDefInstanceEqualityAfterSessionCreation() throws Exception {
-        // This test ensures the instanceXml from the formDef saved to the FormSession prior to initialization
-        // is the same as the instanceXml off of a freshly created FormDef from the xml file
-        NewFormResponse newSessionResponse = startNewForm("requests/new_form/new_form_3.json",
-                "xforms/cases/create_case.xml");
-
-        String formXml = FileUtils.getFile(this.getClass(), "xforms/cases/create_case.xml");
+    public void testFormDefSavedPriorToInitialization() throws Exception {
+        // This test ensures a FormDef is saved to a FormSession prior to form def initialization code being run
+        // by checking that the instanceXml matches a formDef's instance made from scratch (rather than through the FormSession creation)
+        String formXml = FileUtils.getFile(this.getClass(), "xforms/hidden_value_form.xml");
         FormDef expectedFormDef = XFormUtils.getFormRaw(new InputStreamReader(IOUtils.toInputStream(formXml, "UTF-8")));
         byte[] expectedInstanceBytes = new XFormSerializingVisitor(false).serializeInstance(expectedFormDef.getInstance());
 
+        NewFormResponse newSessionResponse = startNewForm("requests/new_form/new_form_3.json",
+                "xforms/hidden_value_form.xml");
 
         SerializableFormSession session = this.formSessionService.getSessionById(newSessionResponse.getSessionId());
         FormDef actualFormDef = FormDefStringSerializer.deserialize(session.getFormXml());
@@ -41,5 +42,40 @@ public class SavedFormDefTest extends BaseTestClass {
         String actualFormDefString = new String(actualInstanceBytes, "US-ASCII");
         String expectedFormDefString = new String(expectedInstanceBytes, "US-ASCII");
         assertEquals(expectedFormDefString, actualFormDefString);
+    }
+
+    @Test
+    public void testIrrelevantDataIsIncludedPriorToSubmission() throws Exception {
+        NewFormResponse newSessionResponse = startNewForm("requests/new_form/new_form_3.json",
+                "xforms/hidden_value_form.xml");
+
+        String sessionId = newSessionResponse.getSessionId();
+
+        // this answer does not satisfy the condition, and therefore the calculated property is irrelevant
+        answerQuestionGetResult("0", "9", sessionId);
+
+        SerializableFormSession session = this.formSessionService.getSessionById(newSessionResponse.getSessionId());
+        FormSession formSession = new FormSession(session, this.restoreFactoryMock, null, this.storageFactoryMock);
+        assertEquals(formSession.getInstanceXml(true), session.getInstanceXml());
+    }
+
+    @Test
+    public void testIrrelevantDataIsNotIncludedAfterSubmission() throws Exception {
+        NewFormResponse newSessionResponse = startNewForm("requests/new_form/new_form_3.json",
+                "xforms/hidden_value_form.xml");
+
+        String sessionId = newSessionResponse.getSessionId();
+
+        // this answer does not satisfy the condition, and therefore the calculated property is irrelevant
+        answerQuestionGetResult("0", "9", sessionId);
+        SubmitResponseBean submitResponseBean = submitForm("requests/submit/submit_hidden_value_form.json", sessionId);
+        assertEquals("success", submitResponseBean.getStatus());
+
+        SerializableFormSession session = this.formSessionService.getSessionById(newSessionResponse.getSessionId());
+        FormSession formSession = new FormSession(session, this.restoreFactoryMock, null, this.storageFactoryMock);
+        // TODO: this is failing because the session.getInstanceXml() appears to still ignore relevance
+        // I'm not sure if it is expected that after a form submission, the form session instanceXml is updated or not
+        // it could just be that we send the instance xml that respects relevance but do not save it to the sesssion now that the session is useless
+        assertEquals(formSession.getInstanceXml(false), session.getInstanceXml());
     }
 }
