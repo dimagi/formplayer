62fd01e31207ef92c0cfefb226a8b47ce20054b6
Shubham Goyal <shubham1g5@gmail.com>
2021-09-24 16:10:47 +0545

    Decouple query screen from case search helper

--- src/main/java/org/commcare/formplayer/services/MenuSessionRunnerService.java
+++ src/main/java/org/commcare/formplayer/services/MenuSessionRunnerService.java
@@ -1,34 +1,19 @@
 package org.commcare.formplayer.services;
 
-import io.sentry.Sentry;
-
-import org.commcare.formplayer.beans.NewFormResponse;
-import org.commcare.formplayer.beans.NotificationMessage;
-import org.commcare.formplayer.beans.menus.*;
-import org.commcare.formplayer.exceptions.ApplicationConfigException;
-import org.commcare.formplayer.objects.QueryData;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.commcare.formplayer.web.client.WebClient;
-import org.commcare.modern.session.SessionWrapper;
-import org.commcare.session.SessionFrame;
-import org.commcare.suite.model.Detail;
-import org.commcare.suite.model.EntityDatum;
-import org.commcare.suite.model.StackFrameStep;
-import org.commcare.suite.model.Text;
-import org.commcare.util.screen.*;
-import org.javarosa.core.model.actions.FormSendCalloutHandler;
-import org.javarosa.core.model.condition.EvaluationContext;
-import org.javarosa.core.model.instance.ExternalDataInstance;
-import org.javarosa.core.model.instance.TreeReference;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.stereotype.Component;
-
+import org.commcare.formplayer.beans.NewFormResponse;
+import org.commcare.formplayer.beans.NotificationMessage;
+import org.commcare.formplayer.beans.menus.BaseResponseBean;
+import org.commcare.formplayer.beans.menus.CommandListResponseBean;
+import org.commcare.formplayer.beans.menus.EntityDetailListResponse;
+import org.commcare.formplayer.beans.menus.EntityDetailResponse;
+import org.commcare.formplayer.beans.menus.EntityListResponse;
+import org.commcare.formplayer.beans.menus.MenuBean;
+import org.commcare.formplayer.beans.menus.QueryResponseBean;
+import org.commcare.formplayer.exceptions.ApplicationConfigException;
 import org.commcare.formplayer.objects.FormVolatilityRecord;
-import org.commcare.formplayer.repo.MenuSessionRepo;
+import org.commcare.formplayer.objects.QueryData;
 import org.commcare.formplayer.screens.FormplayerQueryScreen;
 import org.commcare.formplayer.screens.FormplayerSyncScreen;
 import org.commcare.formplayer.session.FormSession;
@@ -37,15 +22,41 @@ import org.commcare.formplayer.util.Constants;
 import org.commcare.formplayer.util.FormplayerDatadog;
 import org.commcare.formplayer.util.FormplayerHereFunctionHandler;
 import org.commcare.formplayer.util.SessionUtils;
+import org.commcare.formplayer.web.client.WebClient;
+import org.commcare.modern.session.SessionWrapper;
+import org.commcare.session.SessionFrame;
+import org.commcare.suite.model.Detail;
+import org.commcare.suite.model.EntityDatum;
+import org.commcare.suite.model.StackFrameStep;
+import org.commcare.suite.model.Text;
+import org.commcare.util.screen.CommCareSessionException;
+import org.commcare.util.screen.EntityScreen;
+import org.commcare.util.screen.MenuScreen;
+import org.commcare.util.screen.QueryScreen;
+import org.commcare.util.screen.Screen;
+import org.javarosa.core.model.actions.FormSendCalloutHandler;
+import org.javarosa.core.model.condition.EvaluationContext;
+import org.javarosa.core.model.instance.ExternalDataInstance;
+import org.javarosa.core.model.instance.TreeReference;
+import org.javarosa.xml.util.InvalidStructureException;
+import org.javarosa.xml.util.UnfullfilledRequirementsException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestClientResponseException;
+import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Vector;
-import java.util.Arrays;
 
 import javax.annotation.Resource;
+
+import io.sentry.Sentry;
 
 /**
  * Class containing logic for accepting a NewSessionRequest and services,
@@ -400,27 +411,39 @@ public class MenuSessionRunnerService {
             screen.answerPrompts(queryDictionary);
         }
 
-        ExternalDataInstance searchDataInstance = searchAndSetResult(
-                screen,
-                screen.getUri(skipDefaultPromptValues));
-
-        if (searchDataInstance != null) {
-            if (screen.getCurrentMessage() != null) {
-                notificationMessage = new NotificationMessage(screen.getCurrentMessage(), false, NotificationMessage.Tag.query);
+        String error = null;
+        ExternalDataInstance searchDataInstance;
+        try {
+            searchDataInstance = searchAndSetResult(
+                    screen,
+                    screen.getUri(skipDefaultPromptValues));
+            if (searchDataInstance == null) {
+                error = "No result from query";
             }
-        } else {
+        } catch (InvalidStructureException | IOException
+                | XmlPullParserException | UnfullfilledRequirementsException e) {
+            e.printStackTrace();
+            error = "Query response format error: " + e.getMessage();
+        }
+
+        if (error != null) {
             notificationMessage = new NotificationMessage(
-                    "Query failed with message " + screen.getCurrentMessage(),
+                    "Query failed with message " + error,
                     true,
                     NotificationMessage.Tag.query);
+        } else {
+            notificationMessage = new NotificationMessage(screen.getCurrentMessage(), false, NotificationMessage.Tag.query);
         }
+
         Screen nextScreen = menuSession.getNextScreen();
         log.info("Next screen after query: " + nextScreen);
         return notificationMessage;
     }
 
-    public ExternalDataInstance searchAndSetResult(FormplayerQueryScreen screen, URI uri) {
-        ExternalDataInstance searchDataInstance = caseSearchHelper.getSearchDataInstance(screen, uri);
+    public ExternalDataInstance searchAndSetResult(FormplayerQueryScreen screen, URI uri)
+            throws UnfullfilledRequirementsException, XmlPullParserException, IOException, InvalidStructureException {
+        ExternalDataInstance searchDataInstance = caseSearchHelper.getSearchDataInstance(screen.getQueryDatum().getDataId(),
+                screen.getQueryDatum().useCaseTemplate(), uri);
         screen.setQueryDatum(searchDataInstance);
         return searchDataInstance;
     }
