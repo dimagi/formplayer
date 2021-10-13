b6a8c25b9a348c83ac30260fe239ee83f2e4dfda
Simon Kelly <skelly@dimagi.com>
2021-10-13 13:53:11 +0200

    resolve queries in a loop before proceeding to the next selection
    If multiple queries are positioned one after another they should each
    be executed if possible. This allows chaining queries together that
    don't require user input.

--- src/test/java/org/commcare/formplayer/tests/FormEntryWithQueryTests.java
+++ src/test/java/org/commcare/formplayer/tests/FormEntryWithQueryTests.java
@@ -20,13 +20,13 @@ import org.springframework.cache.CacheManager;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Hashtable;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @WebMvcTest
 @ContextConfiguration(classes = TestContext.class)
@@ -88,10 +88,91 @@ public class FormEntryWithQueryTests extends BaseTestClass{
         assert uris.get(2).equals(new URI("http://localhost:8000/a/test-1/phone/registry_case/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case&case_id=0156fa3e-093e-4136-b95c-01b13dae66c6"));
 
         // see if the instance is retained into the form session
-        EvaluateXPathResponseBean evaluateXPathResponseBean = evaluateXPath(formResponse.getSessionId(),
-                "instance('registry')/results/case[@case_type='case']/case_name");
+        checkXpath(
+                formResponse,
+                "instance('registry')/results/case[@case_type='case']/case_name",
+                "Burt Maclin"
+        );
+    }
+
+    @Test
+    public void testNavigationToFormEntryWithMultipleQueries() throws Exception {
+        // select module 2
+        sessionNavigateWithQuery(new String[]{"2"},
+                "caseclaimquery",
+                null,
+                false,
+                QueryResponseBean.class);
+
+
+        Hashtable<String, String> inputs = new Hashtable<>();
+        QueryData queryData = new QueryData();
+        queryData.setInputs("m2", inputs);
+        queryData.setExecute("m2", true);
+
+        // execute search query
+        sessionNavigateWithQuery(new String[]{"2"},
+                "caseclaimquery",
+                queryData,
+                false,
+                EntityListResponse.class);
+
+        // Check if form's query was executed
+        verify(webClientMock, times(1)).get(uriCaptor.capture());
+        List<URI> uris = uriCaptor.getAllValues();
+        // when default search, prompts doesn't get included
+        assert uris.get(0).equals(new URI("http://localhost:8000/a/test-1/phone/search/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case"));
+
+        // Select a case
+        CommandListResponseBean menuResponse = sessionNavigateWithQuery(new String[]{"2", "0156fa3e-093e-4136-b95c-01b13dae66c6"},
+                "caseclaimquery",
+                queryData,
+                false,
+                CommandListResponseBean.class);
+
+        assertEquals(1, menuResponse.getCommands().length);
+        assertEquals("Dedupe Form", menuResponse.getCommands()[0].getDisplayText());
+
+        // verify the second query block to fetch the remote case was executed as well as the 3rd query block
+        // to do a custom lookup
+        verify(webClientMock, times(3)).get(uriCaptor.capture());
+        uris = uriCaptor.getAllValues();
+        // when default search, prompts doesn't get included
+        assert uris.get(2).equals(new URI("http://localhost:8000/a/test-1/phone/registry_case/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case&case_id=0156fa3e-093e-4136-b95c-01b13dae66c6"));
+        assert uris.get(3).equals(new URI("http://localhost:8000/a/test-1/phone/registry_case/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case&case_id=dupe_case_id"));
+
+        // Open the form
+        NewFormResponse formResponse = sessionNavigateWithQuery(new String[]{"2", "0156fa3e-093e-4136-b95c-01b13dae66c6", "0"},
+                "caseclaimquery",
+                queryData,
+                false,
+                NewFormResponse.class);
+
+        verify(webClientMock, times(3)).get(uriCaptor.capture());
+        uris = uriCaptor.getAllValues();
+        assert uris.get(2).equals(new URI("http://localhost:8000/a/test-1/phone/registry_case/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case&case_id=0156fa3e-093e-4136-b95c-01b13dae66c6"));
+        assert uris.get(3).equals(new URI("http://localhost:8000/a/test-1/phone/registry_case/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case&case_id=dupe_case_id"));
+
+        // check we can access the 'registry' instance in the form
+        checkXpath(
+                formResponse,
+                "instance('registry')/results/case[@case_type='case']/case_name",
+                "Burt Maclin"
+        );
+
+        // check we can access the 'duplicate' instance in the form
+        checkXpath(
+                formResponse,
+                "instance('duplicate')/results/case[@case_id='dupe_case_id']/case_name",
+                "Duplicate of Burt"
+        );
+    }
+
+    private void checkXpath(NewFormResponse formResponse, String xpath, String expectedValue) throws Exception {
+        EvaluateXPathResponseBean evaluateXPathResponseBean = evaluateXPath(formResponse.getSessionId(), xpath);
         assert evaluateXPathResponseBean.getStatus().equals(Constants.ANSWER_RESPONSE_STATUS_POSITIVE);
-        String result = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<result>Burt Maclin</result>\n";
+        String result = String.format(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<result>%s</result>\n", expectedValue);
         assert evaluateXPathResponseBean.getOutput().equals(result);
     }
 
@@ -100,8 +181,17 @@ public class FormEntryWithQueryTests extends BaseTestClass{
         return "restores/caseclaim.xml";
     }
 
-    private void configureQueryMock() {
-        when(webClientMock.get(any(URI.class)))
-                .thenReturn(FileUtils.getFile(this.getClass(), "query_responses/case_claim_response.xml"));
+    private void configureQueryMock() throws URISyntaxException {
+        URI searchURI = new URI("http://localhost:8000/a/test-1/phone/search/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case");
+        String searchResponse = "query_responses/case_claim_response.xml";
+        when(webClientMock.get(eq(searchURI))).thenReturn(FileUtils.getFile(this.getClass(), searchResponse));
+
+        URI firstQueryURI = new URI("http://localhost:8000/a/test-1/phone/registry_case/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case&case_id=0156fa3e-093e-4136-b95c-01b13dae66c6");
+        String firstQueryResponse = "query_responses/case_claim_response.xml";
+        when(webClientMock.get(eq(firstQueryURI))).thenReturn(FileUtils.getFile(this.getClass(), firstQueryResponse));
+
+        URI secondQueryURI = new URI("http://localhost:8000/a/test-1/phone/registry_case/dec220eae9974c788654f23320f3a8d3/?commcare_registry=shubham&case_type=case&case_id=dupe_case_id");
+        String secondQueryResponse = "query_responses/registry_query_response.xml";
+        when(webClientMock.get(eq(secondQueryURI))).thenReturn(FileUtils.getFile(this.getClass(), secondQueryResponse));
     }
 }
