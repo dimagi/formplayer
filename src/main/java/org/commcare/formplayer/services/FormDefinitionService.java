b1f37293da87900c1604f93938f1be655b1a9eeb
Graham Herceg <g.a.herceg@gmail.com>
2022-01-04 16:59:24 -0800

    add cacheable annotation to getOrCreate method

--- src/main/java/org/commcare/formplayer/services/FormDefinitionService.java
+++ src/main/java/org/commcare/formplayer/services/FormDefinitionService.java
@@ -6,11 +6,14 @@ import org.commcare.formplayer.exceptions.MultipleFormDefsFoundException;
 import org.commcare.formplayer.objects.FormDefinition;
 import org.commcare.formplayer.repo.FormDefinitionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@CacheConfig(cacheNames = {"form_definition"})
 public class FormDefinitionService {
 
     private final Log log = LogFactory.getLog(FormDefinitionService.class);
@@ -18,6 +21,8 @@ public class FormDefinitionService {
     @Autowired
     private FormDefinitionRepo formDefinitionRepo;
 
+
+    @Cacheable
     public FormDefinition getOrCreateFormDefinition(String appId, String appVersion, String formXmlns, String serializedFormDef) {
         List<FormDefinition> formDefs = this.formDefinitionRepo.findByAppIdAndAppVersionAndXmlns(appId, appVersion, formXmlns);
         FormDefinition formDefinition;
