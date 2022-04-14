b622423dfa2d88f017239d84a6c4fccd7f2dc7fa
AddisonDunn <dunnaddisonw@gmail.com>
2022-04-14 12:41:44 -0400

    add new client case claim post method to not sync on 204

--- src/main/java/org/commcare/formplayer/services/MenuSessionRunnerService.java
+++ src/main/java/org/commcare/formplayer/services/MenuSessionRunnerService.java
@@ -482,8 +482,9 @@ public class MenuSessionRunnerService {
     }
 
     private NotificationMessage doSync(FormplayerSyncScreen screen) throws Exception {
+        Boolean shouldSync = true;
         try {
-            webClient.post(screen.getUrl(), screen.getQueryParams());
+            shouldSync = webClient.caseClaimPost(screen.getUrl(), screen.getQueryParams());
         } catch (RestClientResponseException e) {
             return new NotificationMessage(
                     String.format("Case claim failed. Message: %s", e.getResponseBodyAsString()), true,
@@ -492,7 +493,9 @@ public class MenuSessionRunnerService {
             return new NotificationMessage("Unknown error performing case claim", true,
                     NotificationMessage.Tag.sync);
         }
-        restoreFactory.performTimedSync(false, false, false);
+        if (shouldSync) {
+            restoreFactory.performTimedSync(false, false, false);
+        }
         return null;
     }
 
