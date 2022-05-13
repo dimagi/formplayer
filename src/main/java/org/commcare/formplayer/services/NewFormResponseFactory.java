769051bc07ddac908bd9526a765e71b50fabc4d4
Graham Herceg <g.a.herceg@gmail.com>
2022-05-13 07:40:24 -0700

    remove redundant parseFormDef

--- src/main/java/org/commcare/formplayer/services/NewFormResponseFactory.java
+++ src/main/java/org/commcare/formplayer/services/NewFormResponseFactory.java
@@ -84,7 +84,7 @@ public class NewFormResponseFactory {
         FormSession formSession = new FormSession(
                 sandbox,
                 serializableFormDefinition,
-                parseFormDef(formXml),
+                formDef,
                 bean.getUsername(),
                 bean.getDomain(),
                 bean.getSessionData().getData(),
