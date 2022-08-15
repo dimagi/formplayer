1d53c4f754816b21801b2d91d36215268de39bd2
Shubham Goyal <shubham1g5@gmail.com>
2022-08-15 16:29:08 +0530

    adds multipart form data header for multipart requests

--- src/main/java/org/commcare/formplayer/services/SubmitService.java
+++ src/main/java/org/commcare/formplayer/services/SubmitService.java
@@ -27,11 +27,11 @@ public class SubmitService {
 
     private CategoryTimingHelper.RecordingTimer submitTimer;
 
-    public String submitForm(String formXml, String submitUrl) {
+    public String submitForm(Object body, String submitUrl) {
         submitTimer = categoryTimingHelper.newTimer(Constants.TimingCategories.SUBMIT_FORM_TO_HQ, restoreFactory.getDomain());
         submitTimer.start();
         try {
-            return webClient.post(submitUrl, formXml);
+            return webClient.post(submitUrl, body, true);
         } finally {
             submitTimer.end().record();
         }
