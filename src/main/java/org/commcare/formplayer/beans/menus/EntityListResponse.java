0eaa67097aeb7739192f99de7d202a11b5f88521
Shubham Goyal <shubham1g5@gmail.com>
2022-10-20 19:01:29 +0200

    removes duplicate call to setPageCount

--- src/main/java/org/commcare/formplayer/beans/menus/EntityListResponse.java
+++ src/main/java/org/commcare/formplayer/beans/menus/EntityListResponse.java
@@ -186,17 +186,16 @@ public class EntityListResponse extends MenuBean {
             List<Entity<TreeReference>> entityList, Detail detail, int casesPerPage, int offset) {
         if (entityList.size() > casesPerPage && !(detail.getNumEntitiesToDisplayPerRow() > 1)) {
             // we're doing pagination
-            setPageCount((int)Math.ceil((double)entityList.size() / casesPerPage));
             return getEntitiesForCurrentPage(entityList, casesPerPage, offset);
         }
         return entityList;
     }
 
-
     @Trace
     private List<Entity<TreeReference>> getEntitiesForCurrentPage(List<Entity<TreeReference>> matched,
             int casesPerPage,
             int offset) {
+        setPageCount((int)Math.ceil((double)matched.size() / casesPerPage));
         if (offset > matched.size()) {
             // Set the offset to last page
             offset = casesPerPage * (getPageCount() - 1);
@@ -209,7 +208,6 @@ public class EntityListResponse extends MenuBean {
             end = matched.size();
             length = end - offset;
         }
-        setPageCount((int)Math.ceil((double)matched.size() / casesPerPage));
         matched = matched.subList(offset, offset + length);
         return matched;
     }
