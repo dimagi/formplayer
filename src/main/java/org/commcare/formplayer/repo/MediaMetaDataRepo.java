d54c9ab9e2f1f4e75dcacab12338b5ba8a3915d1
robert-costello <robertcostello81@gmail.com>
2022-12-13 10:57:25 -0500

    add interface for MediaMetadataRecord

---
+++ src/main/java/org/commcare/formplayer/repo/MediaMetaDataRepo.java
@@ -0,0 +1,24 @@
+package org.commcare.formplayer.repo;
+
+import org.commcare.formplayer.objects.MediaMetadataRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+/**
+ * JpaRepository interface for {@link MediaMetadataRecord}
+ */
+public interface MediaMetaDataRepo extends JpaRepository<MediaMetadataRecord, String> {
+
+    Optional<MediaMetadataRecord> findByFormSessionId(String formSessionId);
+
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM MediaMetadataRecord WHERE formSessionId IS NULL")
+    int deleteMetaDataWithoutFormSessionId(@Param("formSessionId") String formSessionId);
+}
