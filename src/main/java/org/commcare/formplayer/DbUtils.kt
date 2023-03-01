27a93bda003d1afe65d9417966273eb91e37b0a8
Shubham Goyal <shubham1g5@gmail.com>
2023-03-02 00:33:15 +0530

    move db wrappers to utils class

--- src/main/java/org/commcare/formplayer/DbUtils.kt
+++ src/main/java/org/commcare/formplayer/DbUtils.kt
@@ -1,8 +1,11 @@
 package org.commcare.formplayer
 
 import org.commcare.formplayer.application.SQLiteProperties
+import org.commcare.formplayer.exceptions.SQLiteRuntimeException
+import org.commcare.formplayer.sqlitedb.SQLiteDB
 import org.commcare.modern.database.TableBuilder
 import java.io.File
+import java.sql.SQLException
 
 /**
  * Utility functions required to construct Sqlite Dbs
@@ -16,10 +19,45 @@ object DbUtils {
         )
     }
 
-
     private fun getUsernameDetail(username: String, asUsername: String?): String {
         return if (asUsername != null) {
             username + "_" + asUsername
         } else username
+    }
+
+    /**
+     * Set auto commit on given sqlite db
+     */
+    @JvmStatic
+    fun setAutoCommit(sqLiteDB: SQLiteDB, autoCommit: Boolean) {
+        try {
+            sqLiteDB.connection.autoCommit = autoCommit
+        } catch (e: SQLException) {
+            throw SQLiteRuntimeException(e)
+        }
+    }
+
+    /**
+     * Rollbacks given sqlite db
+     */
+    @JvmStatic
+    fun rollback(sqLiteDB: SQLiteDB) {
+        try {
+            sqLiteDB.connection.rollback()
+        } catch (e: SQLException) {
+            throw SQLiteRuntimeException(e)
+        }
+    }
+
+    /**
+     * Commits given sqlite db
+     */
+    @JvmStatic
+    fun commit(sqLiteDB: SQLiteDB) {
+        try {
+            sqLiteDB.connection.commit()
+        } catch (e: SQLException) {
+            throw SQLiteRuntimeException(e)
+        }
     }
 }
