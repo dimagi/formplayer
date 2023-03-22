844d04c8d8a3f378906be4159da53a7b99b2acb1
robert-costello <robertcostello81@gmail.com>
2023-03-22 14:55:36 -0400

    make SUPPORTED_FILE_EXTS uniform

--- src/main/java/org/commcare/formplayer/services/MediaValidator.kt
+++ src/main/java/org/commcare/formplayer/services/MediaValidator.kt
@@ -12,7 +12,7 @@ object MediaValidator {
     // 3 MB size limit
     private const val MAX_BYTES_PER_ATTACHMENT = (3 * 1048576 - 1024).toLong()
     private val SUPPORTED_FILE_EXTS = ImmutableList.of(
-        ".jpg",
+        "jpg",
         "jpeg",
         "png",
         "pdf",
