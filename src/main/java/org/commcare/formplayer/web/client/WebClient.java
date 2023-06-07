787a68b0d18fc25771b893c17bf83bb7c9000489
Shubham Goyal <shubham1g5@gmail.com>
2023-06-07 12:39:30 -0400

    change ip header to X-CommCareHQ-Origin-IP

--- src/main/java/org/commcare/formplayer/web/client/WebClient.java
+++ src/main/java/org/commcare/formplayer/web/client/WebClient.java
@@ -64,7 +64,7 @@ public class WebClient {
         if (isMultipart) {
             headers.setContentType(MediaType.MULTIPART_FORM_DATA);
         }
-        headers.add("X-FORWARDED-FOR", RequestUtils.getIpAddress());
+        headers.add("X-CommCareHQ-Origin-IP", RequestUtils.getIpAddress());
         return postRaw(uri, headers, body, String.class).getBody();
     }
 
