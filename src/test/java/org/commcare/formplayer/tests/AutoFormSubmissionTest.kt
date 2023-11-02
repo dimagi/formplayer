2d0f0f07c3d4e4604d86aa4f75077ece3c97320c
Shubham Goyal <shubham1g5@gmail.com>
2023-11-02 09:57:05 -0500

    Adds test for endpoint launch to auto submit form

--- src/test/java/org/commcare/formplayer/tests/AutoFormSubmissionTest.kt
+++ src/test/java/org/commcare/formplayer/tests/AutoFormSubmissionTest.kt
@@ -1,6 +1,9 @@
 package org.commcare.formplayer.tests
 
+import org.commcare.formplayer.beans.NewFormResponse
 import org.commcare.formplayer.beans.SubmitResponseBean
+import org.commcare.formplayer.util.Constants
+import org.commcare.formplayer.utils.WithHqUser
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
@@ -14,10 +17,23 @@ class AutoFormSubmissionTest : BaseTestClass() {
     }
 
     @Test
-    fun testAutoSelection() {
+    fun testAutoFormSubmission() {
         // We directly go to the form without selecting the case
         val selections = arrayOf("2", "0")
         val response = sessionNavigate(selections, APP, SubmitResponseBean::class.java)
+        assertNotNull(response)
+        assertEquals("success", response.status)
+    }
+
+    @Test
+    @WithHqUser(enabledToggles = [Constants.TOGGLE_SESSION_ENDPOINTS])
+    fun testAutoSubmitFormEndpoint() {
+        val response = sessionNavigateWithEndpoint(
+            APP,
+            "auto_submit_form",
+            HashMap(),
+            SubmitResponseBean::class.java
+        )
         assertNotNull(response)
         assertEquals("success", response.status)
     }
