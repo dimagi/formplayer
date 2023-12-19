d5b2028598ca9cd3d172c615a4ab892d18a1f6d3
Shubham Goyal <shubham1g5@gmail.com>
2023-12-19 13:20:22 -0500

    clear up static state after test

--- src/test/java/org/commcare/formplayer/junit/InitializeStaticsExtension.java
+++ src/test/java/org/commcare/formplayer/junit/InitializeStaticsExtension.java
@@ -4,16 +4,18 @@ import org.commcare.formplayer.application.SQLiteProperties;
 import org.commcare.formplayer.engine.ClasspathFileRoot;
 import org.commcare.formplayer.util.PrototypeUtils;
 import org.javarosa.core.model.utils.DateUtils;
+import org.javarosa.core.reference.ReferenceHandler;
 import org.javarosa.core.reference.ReferenceManager;
 import org.javarosa.core.services.locale.Localization;
 import org.javarosa.core.services.locale.LocalizerManager;
+import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 /**
  * Perform initialization of static utils.
  */
-public class InitializeStaticsExtension implements BeforeEachCallback {
+public class InitializeStaticsExtension implements BeforeEachCallback, AfterEachCallback {
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
         LocalizerManager.setUseThreadLocalStrategy(true);
@@ -25,5 +27,11 @@ public class InitializeStaticsExtension implements BeforeEachCallback {
 
         PrototypeUtils.setupThreadLocalPrototypes();
         DateUtils.setTimezoneProvider(new MockTimezoneProvider());
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        ReferenceHandler.clearInstance();
+        LocalizerManager.clearInstance();
     }
 }
