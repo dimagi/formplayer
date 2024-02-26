b816b7bf8ee2f8d5303183b5c622b272480e0ed9
Jonathan Tang <Jonahtang738@gmail.com>
2024-02-26 10:55:50 -0800

    feat: adds tag to datadog traces for restore attempts and app installs

--- src/main/java/org/commcare/formplayer/services/ResponseMetaDataTracker.java
+++ src/main/java/org/commcare/formplayer/services/ResponseMetaDataTracker.java
@@ -1,11 +1,15 @@
 package org.commcare.formplayer.services;
 
+import io.opentracing.Span;
+import io.opentracing.util.GlobalTracer;
 import org.commcare.formplayer.util.FormplayerDatadog;
 import org.commcare.formplayer.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
+
+import datadog.trace.api.interceptor.MutableSpan;
 
 @Component
 @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
@@ -23,6 +27,11 @@ public class ResponseMetaDataTracker {
     }
 
     public void setAttemptRestore(boolean attemptRestore) {
+        final Span span = GlobalTracer.get().activeSpan();
+        if (span != null && (span instanceof MutableSpan)) {
+            MutableSpan localRootSpan = ((MutableSpan) span).getLocalRootSpan();
+            localRootSpan.setTag("attempt_restore", attemptRestore);
+        }
         datadog.addRequestScopedTag(Constants.CATEGORY_TAG, Constants.TimingCategories.COMPLETE_RESTORE);
         this.attemptRestore = attemptRestore;
     }
@@ -32,6 +41,11 @@ public class ResponseMetaDataTracker {
     }
 
     public void setNewInstall(boolean newInstall) {
+        final Span span = GlobalTracer.get().activeSpan();
+        if (span != null && (span instanceof MutableSpan)) {
+            MutableSpan localRootSpan = ((MutableSpan) span).getLocalRootSpan();
+            localRootSpan.setTag("new_install", newInstall);
+        }
         datadog.addRequestScopedTag(Constants.CATEGORY_TAG, Constants.TimingCategories.APP_INSTALL);
         this.newInstall = newInstall;
     }
