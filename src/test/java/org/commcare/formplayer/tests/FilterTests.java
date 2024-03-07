eb7fa3ae36a8ebb9bea04cd3a5df4557128cca44
Jonathan Tang <Jonahtang738@gmail.com>
2024-03-07 13:29:21 -0800

    test: migrate to new style of test with more standard Spring Boot architecture

--- src/test/java/org/commcare/formplayer/tests/FilterTests.java
+++ src/test/java/org/commcare/formplayer/tests/FilterTests.java
@@ -1,38 +1,69 @@
 package org.commcare.formplayer.tests;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import org.commcare.cases.model.Case;
 import org.commcare.formplayer.application.SQLiteProperties;
+import org.commcare.formplayer.application.UtilController;
 import org.commcare.formplayer.beans.SyncDbResponseBean;
+import org.commcare.formplayer.configuration.CacheConfiguration;
+import org.commcare.formplayer.junit.InitializeStaticsExtension;
 import org.commcare.formplayer.sandbox.SqlSandboxUtils;
 import org.commcare.formplayer.sandbox.SqlStorage;
 import org.commcare.formplayer.sandbox.UserSqlSandbox;
+import org.commcare.formplayer.services.FormplayerLockRegistry;
+import org.commcare.formplayer.services.RestoreFactory;
+import org.commcare.formplayer.sqlitedb.UserDB;
 import org.commcare.formplayer.util.Constants;
 import org.commcare.formplayer.utils.TestContext;
+import org.commcare.formplayer.web.client.WebClient;
+import org.commcare.formplayer.junit.RestoreFactoryExtension;
+import org.commcare.formplayer.junit.request.SyncDbRequest;
+import org.commcare.formplayer.junit.request.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
-
+import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest
-public class FilterTests extends BaseTestClass {
+@Import({UtilController.class})
+@ContextConfiguration(classes = {TestContext.class, CacheConfiguration.class})
+@ExtendWith(InitializeStaticsExtension.class)
+public class FilterTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private RestoreFactory restoreFactoryMock;
+
+    @MockBean
+    private FormplayerLockRegistry lockRegistry;
+
+    @RegisterExtension
+    static RestoreFactoryExtension restoreFactoryExt = new RestoreFactoryExtension.builder()
+            .withUser("test").withDomain("test")
+            .withRestorePath("test_restore.xml")
+            .build();
 
     @Test
     public void testSyncDb() throws Exception {
+        SyncDbRequest syncDbRequest = new SyncDbRequest(mockMvc, restoreFactoryMock);
+        Response response = syncDbRequest.request();
+        response.andExpect(jsonPath("status").value("accepted"));
 
-        configureRestoreFactory("synctestdomain", "synctestuser");
-
-        SyncDbResponseBean syncDbResponseBean = syncDb();
-
-        assert (syncDbResponseBean.getStatus().equals(Constants.ANSWER_RESPONSE_STATUS_POSITIVE));
         assert (SqlSandboxUtils.databaseFolderExists(SQLiteProperties.getDataDir()));
-
-        UserSqlSandbox sandbox = new UserSqlSandbox(
-                getUserDbConnector("synctestdomain", "synctestuser", null));
+        UserDB customConnector = new UserDB("test", "test", null);
+        UserSqlSandbox sandbox = new UserSqlSandbox(customConnector);
 
         SqlStorage<Case> caseStorage = sandbox.getCaseStorage();
 
         assert (15 == caseStorage.getNumRecords());
 
-        //TODO add ledgers, fixtures, etc.
+        // TODO add ledgers, fixtures, etc.
     }
 
     @Test
