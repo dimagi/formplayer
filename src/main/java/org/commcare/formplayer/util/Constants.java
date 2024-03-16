6260f32b5a4d4e6ce158505296b8b9dd93f444b2
Jonathan Tang <Jonahtang738@gmail.com>
2024-03-15 17:16:21 -0700

    delete repeat variable

--- src/main/java/org/commcare/formplayer/util/Constants.java
+++ src/main/java/org/commcare/formplayer/util/Constants.java
@@ -133,7 +133,6 @@ public class Constants {
     public static final String DURATION_TAG = "duration";
     public static final String REQUEST_INITIATED_BY_TAG_PREFIX = "request_initiated_by_";
 
-    public static final String TAG_VALUE_TRUE = "true";
 
 
     // Datadog tags assigned true/false values
