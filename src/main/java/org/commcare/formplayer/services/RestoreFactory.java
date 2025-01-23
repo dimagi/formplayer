1f967ab14fab24d24485feae870180edf228b47a
Minha <mlee@dimagi.com>
2025-01-23 13:50:32 -0500

    Typo fix

--- src/main/java/org/commcare/formplayer/services/RestoreFactory.java
+++ src/main/java/org/commcare/formplayer/services/RestoreFactory.java
@@ -338,7 +338,7 @@ public class RestoreFactory {
                             break;
                         }
                     }
-                    if (oldSandboxLocations.equals(newSandboxLocations)) {
+                    if (!oldSandboxLocations.equals(newSandboxLocations)) {
                         hasLocationChanged = true;
                     }
                 }
