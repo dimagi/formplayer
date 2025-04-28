de19f994c371a3c5735d46df46ee09d3f81d25f9
Jonathan Tang <Jonahtang738@gmail.com>
2025-04-28 16:12:22 -0700

    feat: add timing for high level submit form and report error to sentry if timing exceeds threshold

--- src/main/java/org/commcare/formplayer/application/FormSubmissionController.java
+++ src/main/java/org/commcare/formplayer/application/FormSubmissionController.java
@@ -1,11 +1,15 @@
 package org.commcare.formplayer.application;
 
+import io.sentry.SentryLevel;
+
 import org.commcare.formplayer.annotations.ConfigureStorageFromSession;
 import org.commcare.formplayer.annotations.UserLock;
 import org.commcare.formplayer.annotations.UserRestore;
 import org.commcare.formplayer.beans.SubmitRequestBean;
 import org.commcare.formplayer.beans.SubmitResponseBean;
+import org.commcare.formplayer.services.CategoryTimingHelper;
 import org.commcare.formplayer.util.Constants;
+import org.commcare.formplayer.util.FormplayerSentry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -28,6 +32,9 @@ public class FormSubmissionController extends AbstractBaseController {
     @Autowired
     private FormSubmissionHelper formSubmissionHelper;
 
+    @Autowired
+    private CategoryTimingHelper categoryTimingHelper;
+
     @RequestMapping(value = Constants.URL_SUBMIT_FORM, method = RequestMethod.POST)
     @ResponseBody
     @UserLock
@@ -36,7 +43,18 @@ public class FormSubmissionController extends AbstractBaseController {
     public SubmitResponseBean submitForm(@RequestBody SubmitRequestBean submitRequestBean,
             @CookieValue(name = Constants.POSTGRES_DJANGO_SESSION_ID, required = false) String authToken,
             HttpServletRequest request) throws Exception {
-        return formSubmissionHelper.processAndSubmitForm(request, submitRequestBean.getSessionId(), submitRequestBean.getDomain(),
+            CategoryTimingHelper.RecordingTimer timer = categoryTimingHelper.newTimer(Constants.TimingCategories.PROCESS_AND_SUBMIT_FORM, submitRequestBean.getDomain());
+            timer.start();
+            try {
+                return formSubmissionHelper.processAndSubmitForm(request, submitRequestBean.getSessionId(), submitRequestBean.getDomain(),
                 submitRequestBean.isPrevalidated(), submitRequestBean.getAnswers(), submitRequestBean.getWindowWidth());
+            } finally {
+                timer.end().record();
+                int timingThreshold = 5000;
+                if (timer.durationInMs() > timingThreshold) {
+                    Exception e = new Exception("Form submission took longer than " + timingThreshold + "ms");
+                    FormplayerSentry.captureException(e, SentryLevel.WARNING);
+                };
+            }
     }
 }
